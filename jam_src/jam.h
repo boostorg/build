7983e2221126f5e8169c4212377c038721362704
Dave Abrahams <dave@boostpro.com>
2001-11-11 13:01:26 +0000

    Added copyright notices
    [SVN r11662]

--- jam_src/jam.h
+++ jam_src/jam.h
@@ -4,6 +4,14 @@
  * This file is part of Jam - see jam.c for Copyright information.
  */
 
+/*  This file is ALSO:
+ *  (C) Copyright David Abrahams 2001. Permission to copy, use,
+ *  modify, sell and distribute this software is granted provided this
+ *  copyright notice appears in all copies. This software is provided
+ *  "as is" without express or implied warranty, and with no claim as
+ *  to its suitability for any purpose.
+ */
+
 /*
  * jam.h - includes and globals for jam
  *
--- jam_src/rules.c
+++ jam_src/rules.c
@@ -13,6 +13,14 @@
 # include "hash.h"
 # include "modules.h"
 
+/*  This file is ALSO:
+ *  (C) Copyright David Abrahams 2001. Permission to copy, use,
+ *  modify, sell and distribute this software is granted provided this
+ *  copyright notice appears in all copies. This software is provided
+ *  "as is" without express or implied warranty, and with no claim as
+ *  to its suitability for any purpose.
+ */
+
 /*
  * rules.c - access to RULEs, TARGETs, and ACTIONs
  *
