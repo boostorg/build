c9c1a714d431f3ab8aba1b8304e4079d612e76e9
Vladimir Prus <ghost@cs.msu.su>
2002-07-02 14:14:45 +0000

    Project root loading tweaks.
        * new/project-root.jam (load): Really produce an error message when
            project root file is not found.
        * test/project_test3.py: Test for the error reporting.
    
    
    [SVN r14280]

--- v2/test/project_test3.py
+++ v2/test/project_test3.py
@@ -5,9 +5,18 @@ import os
 from string import strip
 
 t = Tester()
+
+# First check some startup
 t.set_tree("project-test3")
+os.remove("project-root.jam")
+t.run_build_system(status=1, stdout=
+"""Failed to find the project root for directory '.'.
+Did not find a project-root.jam file there or in any of its parent directories.
+Please consult the documentation at 'http://www.boost.org'.
+""")
 
 
+t.set_tree("project-test3")
 t.run_build_system()
 
 t.expect_addition("bin/a.obj")
