e21fb49bcf9280dcfc4518612a57e20ff323efd5
Vladimir Prus <ghost@cs.msu.su>
2002-07-02 15:02:04 +0000

    Implemented getting build request from the command line.
        * new/build-request.jam (from-command-line): New rule.
        * new/build-system.jam: Get build request from the command line,
            expand it and pass to 'generate' rules.
        * new/targets.jam: Use targets's properties when computing path.
        * test/project_test3.py: Test the above changes.
    
    
    [SVN r14283]

--- test/project_test3.py
+++ test/project_test3.py
@@ -19,18 +19,24 @@ Please consult the documentation at 'http://www.boost.org'.
 t.set_tree("project-test3")
 t.run_build_system()
 
-t.expect_addition("bin/a.obj")
-t.expect_content("bin/a.obj",
+t.expect_addition("bin/debug/a.obj")
+t.expect_content("bin/debug/a.obj",
 """debug
 a.cpp
 """)
 
-t.expect_addition("bin/a.exe")
-t.expect_content("bin/a.exe",
+t.expect_addition("bin/debug/a.exe")
+t.expect_content("bin/debug/a.exe",
 """debug
-bin/a.obj
+bin/debug/a.obj
 """)
 
 t.touch("a.cpp")
 t.run_build_system()
-t.expect_touch(["bin/a.obj", "bin/a.exe"])
+t.expect_touch(["bin/debug/a.obj", "bin/debug/a.exe"])
+
+
+t.run_build_system(extra_args="release optimization=off,on")
+t.expect_addition(["bin/release/a.exe", "bin/release/a.obj", 
+		    "bin/release/optimization-off/a.exe", 
+		    "bin/release/optimization-off/a.obj"])
