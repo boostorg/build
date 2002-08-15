0d6ed73e1d0579d17ed25c6a617d9a789082ae7b
Vladimir Prus <ghost@cs.msu.su>
2002-08-15 09:29:49 +0000

    Created a new file 'common.jam' to keep rules not specific to a toolset.
        * new/targets.jam: Use 'common' instead of 'utility'.
        * test/project_test3.py: Test that 'clean' works.
        * test/svn_tree.py: Sync with upstream revision 2944. Fix a bug
           which caused empty directory to be treated as file.
    
    
    [SVN r14886]

--- v2/test/svn_tree.py
+++ v2/test/svn_tree.py
@@ -26,6 +26,97 @@ import os
 import stat
 
 
+
+#========================================================================
+
+# ===>  Overview of our Datastructures  <===
+
+# The general idea here is that many, many things can be represented by
+# a tree structure:
+
+#   - a working copy's structure and contents
+#   - the output of 'svn status'
+#   - the output of 'svn checkout/update'
+#   - the output of 'svn commit'
+
+# The idea is that a test function creates a "expected" tree of some
+# kind, and is then able to compare it to an "actual" tree that comes
+# from running the Subversion client.  This is what makes a test
+# automated; if an actual and expected tree match exactly, then the test
+# has passed.  (See compare_trees() below.)
+
+# The SVNTreeNode class is the fundamental data type used to build tree
+# structures.  The class contains a method for "dropping" a new node
+# into an ever-growing tree structure. (See also create_from_path()).
+
+# We have four parsers in this file for the four use cases listed above:
+# each parser examines some kind of input and returns a tree of
+# SVNTreeNode objects.  (See build_tree_from_checkout(),
+# build_tree_from_commit(), build_tree_from_status(), and
+# build_tree_from_wc()).  These trees are the "actual" trees that result
+# from running the Subversion client.
+
+# Also necessary, of course, is a convenient way for a test to create an
+# "expected" tree.  The test *could* manually construct and link a bunch
+# of SVNTreeNodes, certainly.  But instead, all the tests are using the
+# build_generic_tree() routine instead.
+
+# build_generic_tree() takes a specially-formatted list of lists as
+# input, and returns a tree of SVNTreeNodes.  The list of lists has this
+# structure:
+
+#   [ ['/full/path/to/item', 'text contents', {prop-hash}, {att-hash}],
+#     [...],
+#     [...],
+#     ...   ]
+
+# You can see that each item in the list essentially defines an
+# SVNTreeNode.  build_generic_tree() instantiates a SVNTreeNode for each
+# item, and then drops it into a tree by parsing each item's full path.
+
+# So a typical test routine spends most of its time preparing lists of
+# this format and sending them to build_generic_tree(), rather than
+# building the "expected" trees directly.
+
+#   ### Note: in the future, we'd like to remove this extra layer of
+#   ### abstraction.  We'd like the SVNTreeNode class to be more
+#   ### directly programmer-friendly, providing a number of accessor
+#   ### routines, so that tests can construct trees directly.
+
+# The first three fields of each list-item are self-explanatory.  It's
+# the fourth field, the "attribute" hash, that needs some explanation.
+# The att-hash is used to place extra information about the node itself,
+# depending on the parsing context:
+
+#   - in the 'svn co/up' use-case, each line of output starts with two
+#     characters from the set of (A, D, G, U, C, _).  This status code
+#     is stored in a attribute named 'status'.
+
+#   - in the 'svn ci/im' use-case, each line of output starts with one
+#      of the words (Adding, Deleting, Sending).  This verb is stored in
+#      an attribute named 'verb'.
+
+#   - in the 'svn status' use-case (which is always run with the -v
+#     (--verbose) flag), each line of output contains a working revision
+#     number and a two-letter status code similar to the 'svn co/up'
+#     case.  The repository revision is also printed.  All of this
+#     information is stored in attributes named 'wc_rev', 'status', and
+#     'repos_rev', respectively.
+
+#   - in the working-copy use-case, the att-hash is ignored.
+
+
+# Finally, one last explanation: the file 'actions.py' contain a number
+# of helper routines named 'run_and_verify_FOO'.  These routines take
+# one or more "expected" trees as input, then run some svn subcommand,
+# then push the output through an appropriate parser to derive an
+# "actual" tree.  Then it runs compare_trees() and returns the result.
+# This is why most tests typically end with a call to
+# run_and_verify_FOO().
+
+
+
+
 # A node in a tree.
 #
 # If CHILDREN is None, then the node is a file.  Otherwise, CHILDREN
@@ -52,22 +143,22 @@ class SVNTreeNode:
   def add_child(self, newchild):
     if self.children is None:  # if you're a file,
       self.children = []     # become an empty dir.
-    n = 0
+    child_already_exists = 0
     for a in self.children:
       if a.name == newchild.name:
-        n = 1
+        child_already_exists = 1
         break
-    if n == 0:  # append child only if we don't already have it.
+    if child_already_exists == 0:
       self.children.append(newchild)
-      newchild.path = os.path.join (self.path, newchild.name)
+      newchild.path = os.path.join (self.path, newchild.name)      
 
     # If you already have the node,
-    else:
+    else:      
       if newchild.children is None:
         # this is the 'end' of the chain, so copy any content here.
         a.contents = newchild.contents
         a.props = newchild.props
-        a.atts = attribute_merge(a.atts, newchild.atts)
+        a.atts = newchild.atts
         a.path = os.path.join (self.path, newchild.name)
       else:
         # try to add dangling children to your matching node
@@ -76,12 +167,16 @@ class SVNTreeNode:
 
 
   def pprint(self):
-    print " * Node name: ", self.name
-    print "    Path:     ", self.path
+    print " * Node name:  ", self.name
+    print "    Path:      ", self.path
     print "    Contents:  ", self.contents
     print "    Properties:", self.props
     print "    Attributes:", self.atts
-    if self.children:
+    ### FIXME: I'd like to be able to tell the difference between
+    ### self.children is None (file) and self.children == [] (empty
+    ### diretory), but it seems that most places that construct
+    ### SVNTreeNode objects don't even try to do that.  --xbc
+    if self.children is not None:
       print "    Children:  ", len(self.children)
     else:
       print "    Children: is a file."
@@ -203,9 +298,6 @@ def create_from_path(path, contents=None, props={}, atts={}):
   return root_node
 
 
-# a regexp machine for matching the name of the administrative dir.
-rm = re.compile("^SVN/|/SVN/|/SVN$|^/SVN/|^SVN$")
-
 # helper for handle_dir(), which is a helper for build_tree_from_wc()
 def get_props(path):
   "Return a hash of props for PATH, using the svn client."
@@ -216,7 +308,7 @@ def get_props(path):
   # respecting the black-box paradigm.
 
   props = {}
-  output, errput = main.run_svn(None, "proplist", path)
+  output, errput = main.run_svn(1, "proplist", path, "--verbose")
 
   for line in output:
     name, value = line.split(' : ')
@@ -275,7 +367,7 @@ def handle_dir(path, current_parent, load_props, ignore_svn):
       dprops = get_props(d)
     else:
       dprops = {}
-    new_dir_node = SVNTreeNode(os.path.basename(d), None, None, dprops)
+    new_dir_node = SVNTreeNode(os.path.basename(d), [], None, dprops)
     handle_dir(d, new_dir_node, load_props, ignore_svn)
     new_dir_node.mtime = os.stat(f)[stat.ST_MTIME]
     current_parent.add_child(new_dir_node)
@@ -308,9 +400,9 @@ def default_singleton_handler(a, baton):
 # Main tree comparison routine!
 
 def compare_trees(a, b,
-                  singleton_handler_a = default_singleton_handler,
+                  singleton_handler_a = None,
                   a_baton = None,
-                  singleton_handler_b = default_singleton_handler,
+                  singleton_handler_b = None,
                   b_baton = None):
   """Compare SVNTreeNodes A and B, expressing differences using FUNC_A
   and FUNC_B.  FUNC_A and FUNC_B are functions of two arguments (a
@@ -324,28 +416,50 @@ def compare_trees(a, b,
   exists in both, call compare_trees on the two entries; otherwise, if
   the entry exists only in A, invoke FUNC_A on it, and likewise for
   B with FUNC_B."""
+
+  def display_nodes(a, b):
+    'Display two nodes, expected and actual.'
+    print "============================================================="
+    print "Expected", b.name, "and actual", a.name, "are different!"
+    print "============================================================="
+    print "EXPECTED NODE TO BE:"
+    print "============================================================="
+    b.pprint()
+    print "============================================================="
+    print "ACTUAL NODE FOUND:"
+    print "============================================================="
+    a.pprint()
+
+  # Setup singleton handlers
+  if (singleton_handler_a is None):
+    singleton_handler_a = default_singleton_handler
+  if (singleton_handler_b is None):
+    singleton_handler_b = default_singleton_handler
+
   try:
     # A and B are both files.
     if ((a.children is None) and (b.children is None)):
       if compare_file_nodes(a, b):
-        print "Unequal nodes from actual",a.name,"and expected",b.name
-        a.pprint()
-        b.pprint()
-        raise SVNTreeUnequal
+        display_nodes(a, b)
+        raise main.SVNTreeUnequal
     # One is a file, one is a directory.
     elif (((a.children is None) and (b.children is not None))
           or ((a.children is not None) and (b.children is None))):
-      a.pprint()
-      b.pprint()
-      raise SVNTypeMismatch
+      display_nodes(a, b)
+      raise main.SVNTypeMismatch
     # They're both directories.
     else:
+      # First, compare the directories' two hashes.
+      if (a.props != b.props) or (a.atts != b.atts):
+        display_nodes(a, b)
+        raise main.SVNTreeUnequal
+
       accounted_for = []
       # For each child of A, check and see if it's in B.  If so, run
       # compare_trees on the two children and add b's child to
       # accounted_for.  If not, run FUNC_A on the child.  Next, for each
-      # child of B, check and and see if it's in accounted_for.  If it
-      # is, do nothing. If not, run FUNC_B on it.
+      # child of B, check and see if it's in accounted_for.  If it is,
+      # do nothing. If not, run FUNC_B on it.
       for a_child in a.children:
         b_child = get_child(b, a_child.name)
         if b_child:
@@ -407,6 +521,11 @@ def dump_tree(n,indent=""):
 
 
 ###################################################################
+###################################################################
+# PARSERS that return trees made of SVNTreeNodes....
+
+
+###################################################################
 # Build an "expected" static tree from a list of lists
 
 
@@ -443,8 +562,8 @@ def build_tree_from_checkout(lines):
   "Return a tree derived by parsing the output LINES from 'co' or 'up'."
 
   root = SVNTreeNode(root_node_name)
-  rm = re.compile ('^(..)\s+(.+)')
-
+  rm = re.compile ('^([MAGCUD_ ][MAGCUD_ ]) (.+)')
+  
   for line in lines:
     match = rm.search(line)
     if match and match.groups():
@@ -463,17 +582,18 @@ def build_tree_from_commit(lines):
   "Return a tree derived by parsing the output LINES from 'ci' or 'im'."
 
   # Lines typically have a verb followed by whitespace then a path.
-  # However, a special case exists: the verb "Added" might be followed
-  # by whitespace, "(bin)", more whitespace, and then the path.
   root = SVNTreeNode(root_node_name)
-  rm = re.compile ('^(\w+)\s+(\(bin\))*\s+(.+)')
-
+  rm1 = re.compile ('^(\w+)\s+(.+)')
+  rm2 = re.compile ('^Transmitting')
+  
   for line in lines:
-    match = rm.search(line)
-    if match and match.groups():
-      new_branch = create_from_path(match.group(3), None, {},
-                                    {'verb' : match.group(1)})
-      root.add_child(new_branch)
+    match = rm2.search(line)
+    if not match:
+      match = rm1.search(line)
+      if match and match.groups():
+        new_branch = create_from_path(match.group(2), None, {},
+                                      {'verb' : match.group(1)})
+        root.add_child(new_branch)
 
   return root
 
@@ -483,6 +603,8 @@ def build_tree_from_commit(lines):
 #   Tree nodes will contain no contents, and these atts:
 #
 #          'status', 'wc_rev', 'repos_rev'
+#             ... and possibly 'locked', 'copied', IFF columns non-empty.
+# 
 
 def build_tree_from_status(lines):
   "Return a tree derived by parsing the output LINES from 'st'."
@@ -495,17 +617,22 @@ def build_tree_from_status(lines):
     repos_rev = match.group(1)
   else:
     repos_rev = '?'
-
-  rm = re.compile ('^(..)(.)([^0-9]+)(\d+|-)\s+(.+)')
+    
+  # Try http://www.wordsmith.org/anagram/anagram.cgi?anagram=ACDRMGU
+  rm = re.compile ('^([MACDRUG_ ][MACDRUG_ ])(.)(.)   .   [^0-9-]+(\d+|-)(.{23})(.+)')
   for line in lines:
     match = rm.search(line)
     if match and match.groups():
-      if match.group(4) != '-': # ignore items that only exist on repos
-        new_branch = create_from_path(match.group(5), None, {},
-                                      {'status' : match.group(1),
-                                       'locked' : match.group(2),
-                                       'wc_rev' : match.group(4),
-                                       'repos_rev' : repos_rev})
+      if match.group(5) != '-': # ignore items that only exist on repos
+        atthash = {'status' : match.group(1),
+                   'wc_rev' : match.group(4),
+                   'repos_rev' : repos_rev}
+        if match.group(2) != ' ':
+          atthash['locked'] = match.group(2)
+        if match.group(3) != ' ':
+          atthash['copied'] = match.group(3)
+        new_branch = create_from_path(match.group(6), None, {}, atthash)
+
       root.add_child(new_branch)
 
   return root
@@ -537,10 +664,7 @@ def build_tree_from_wc(wc_path, load_props=0, ignore_svn=1):
 
     return root
 
-
-
-
 ### End of file.
 # local variables:
-# eval: (load-file "../../../../svn-dev.el")
+# eval: (load-file "../../../../../tools/dev/svn-dev.el")
 # end:
