e7eab4c7528ee75bacacebf67c96f67a1d6daa2c
Vladimir Prus <ghost@cs.msu.su>
2002-09-13 12:19:42 +0000

    Prevent creating two equivivalent virtual targets. This, in particular, prevents complining the same source twice with the same properties. Also allow generators to change source names using patterns.
        * new/generators.jam (generator): Accept name patterns together with
            target types.
            (generator.generated-targets): Use name patterns. Transform generated
            targets with 'virtual-targets.register', to eliminate duplicate
            virtual targets.
    
    
    [SVN r15302]

--- test/generators_test.py
+++ test/generators_test.py
@@ -8,7 +8,8 @@ t.set_tree("generators-test")
 t.run_build_system()
 
 t.expect_addition("bin/gcc/debug/threading-single/" * List(   
-    "a.o b.o c.h c.cpp c.o d.whl d.dlp d.cpp d.lr0 d.o x.c x.o y.x1 y.x2 " +
+    "a.o b.o c.h c.cpp c.o d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp " +
+    "d_parser.lr0 x.c x.o y.x1 y.x2 " +
     "y.cpp y.o a e.marked.cpp e.positions e.target.cpp e.o e"))
 
 t.expect_addition(["lib/bin/gcc/debug/threading-single/c.o", "lib/bin/gcc/debug/threading-single/auxilliary"])
