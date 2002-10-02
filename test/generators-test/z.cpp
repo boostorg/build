4af9e4371fd11097e576766c407b9eb257442307
Vladimir Prus <ghost@cs.msu.su>
2002-10-02 12:38:13 +0000

    Test for poor main's per-source requirements.
        * new/builtin.jam: Make 'obj' main type.
        * new/generators-test/*: Try compiling the same CPP file with
            different defines and linking in the same exe.
    
    
    [SVN r15649]

---
+++ test/generators-test/z.cpp
@@ -0,0 +1,8 @@
+
+#if SELECT == 1
+void z1() {}
+#elif SELECT == 2
+void z2() {}
+#else
+#error Invlid value of SELECT
+#endif
--- v2/test/generators-test/a.cpp
+++ v2/test/generators-test/a.cpp
@@ -2,9 +2,13 @@
 int foo();
 int bar();
 
+void z1(), z2();
+
 int main()
 {
     foo();
     bar();
+    z1();
+    z2();
     return 0;
 }
