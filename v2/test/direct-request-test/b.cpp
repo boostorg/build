18991908fc12a7e2b07026cb21dd8c1f71c5c68e
Vladimir Prus <ghost@cs.msu.su>
2002-10-03 15:18:59 +0000

    Propagate free properties in build request to all directly requested targets.
        * targets.jam
            (abstract-target.direct-build-request): New rule.
            (project-target.direct-build-request): New rule.
            (main-target.direct-build-request): New rule.
            (main-target.generate): Use data stored by
            'direct-build-request' to adjust build properties.
    
        * build-system.jam: Call 'direct-build-request' on project
             target in "."
    
        * feature.jam (feature): Don't allow propagated free features.
    
    
    [SVN r15677]

---
+++ v2/test/direct-request-test/b.cpp
@@ -0,0 +1,4 @@
+
+#ifdef MACROS
+void foo() {}
+#endif
---
+++ v2/test/direct_request_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from BoostBuild import Tester, List
+import os
+from string import strip
+
+t = Tester()
+
+# First check some startup
+t.set_tree("direct-request-test")
+t.run_build_system(extra_args="define=MACROS")
+
+t.expect_addition("bin/gcc/debug/shared-false/threading-single/" * List("a.o b.o b.a a"))
+
+t.cleanup()
