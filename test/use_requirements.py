b55dc66bf3018bebb2384880ff31085c1bfe46d7
Vladimir Prus <ghost@cs.msu.su>
2002-10-09 11:33:22 +0000

    Implement simple form of use requirements.
        * new/virtual-target.jam
            (virtual-target.use-requirements): New rule.
            (virtual-target.set-use-requirements): New rule.
    
        * new/generators.jam (construct): Use 'use-requirements' of sources.
    
        * new/targets.jam (basic-target.generate): Store use requirements for
                 generated targets.
          (main-target-alternative): Process use requirements.
    
        * new/project.jam: Support 'use-requirements' project attribute.
    
    
    [SVN r15821]

---
+++ test/use_requirements.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+from BoostBuild import Tester
+t = Tester()
+
+# Test that use requirements on main target work
+t.write("project-root.jam", "import gcc ;")
+t.write("Jamfile", """
+    lib b : b.cpp : : : <define>FOO ;
+    exe a : a.cpp b ;
+""")
+t.write("b.cpp", "void foo() {}")
+t.write("a.cpp", """
+#ifdef FOO
+void foo() {}
+#endif
+int main() { foo(); }
+""")
+
+t.run_build_system()
+
+t.run_build_system("--clean")
+
+# Test that use requirement on project work
+t.write("Jamfile", "exe a : a.cpp lib/b ;")
+t.write("lib/Jamfile", """
+project :
+    use-requirements <define>FOO
+    ;
+lib b : b.cpp ;
+""")
+t.write("lib/b.cpp", "void foo() {}")
+t.run_build_system()
+
+# Test that use requirements are inherited correctly
+
+t.write("Jamfile", "exe a : a.cpp lib/1/b ;")
+
+t.write("a.cpp", """
+#if defined(FOO) && defined(ZOO)
+void foo() {}
+#endif
+int main() { foo(); }
+""")
+
+t.write("lib/Jamfile", """
+project :
+    use-requirements <define>FOO
+    ;
+""")
+
+t.write("lib/1/Jamfile", """
+project :
+    use-requirements <define>ZOO
+    ;
+lib b : b.cpp ;
+""")
+t.write("lib/1/b.cpp", "void foo() {}")
+
+t.run_build_system()
+
+
+
+t.cleanup()
