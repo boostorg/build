89050020265f1b633e250245a9eb1f5773099d20
Vladimir Prus <ghost@cs.msu.su>
2002-12-10 07:48:56 +0000

    Bugfix: dependency features in use requirements were mishandled.
    * new/targets.jam
        (basic-target.generate): Use 'generate-dependencies'
        for use requirements on this target.
    
    * test/use_requirements.py: More tests.
    
    
    [SVN r16580]

--- test/use_requirements.py
+++ test/use_requirements.py
@@ -77,7 +77,51 @@ lib b : b.cpp ;
 t.write("lib/1/b.cpp", "void foo() {}")
 
 t.run_build_system()
+t.run_build_system("--clean")
 
+# Test that we correctly handle dependency features
+# in use requirements on target
 
+t.write("Jamfile", """
+    lib b : b.cpp : : : <define>FOO ;
+    # Here's the test: we should correctly
+    # handle dependency feature and get
+    # use requirements from 'b'.
+    lib c : c.cpp : : : <library>b ;
+    # This will build only if <define>FOO
+    # was propagated from 'c'.
+    exe a : a.cpp c ;
+""")
+t.write("a.cpp", """
+#ifdef FOO
+void foo();
+#endif
+int main() { foo(); }
+""")
+t.write("c.cpp", "")
+
+t.run_build_system()
+t.run_build_system("--clean")
+
+# Test correct handling of dependency features in 
+# project requirements.
+t.write("Jamfile", """
+    exe a : a.cpp lib1/c ;
+""")
+
+t.write("lib1/Jamfile", """
+    project
+        : use-requirements <library>../lib2/b 
+        ;
+    lib c : c.cpp ;    
+""")
+t.write("lib1/c.cpp", "")
+
+t.write("lib2/Jamfile", """
+    lib b : b.cpp : : : <define>FOO ;
+""")
+t.copy("b.cpp", "lib2/b.cpp")
+
+t.run_build_system()
 
 t.cleanup()
