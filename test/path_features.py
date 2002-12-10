84fa4e2c8841c63e4c028a5caf8aa8f8067d67cc
Vladimir Prus <ghost@cs.msu.su>
2002-12-10 07:15:33 +0000

    Bugfix: path features in target requirement were not translated.
    * test/path_features.py: Fix a bug in test.
    
    * new/targets.jam
        (main-target-requirements): Bugfix.
    
    
    [SVN r16579]

--- test/path_features.py
+++ test/path_features.py
@@ -5,7 +5,10 @@ t = Tester()
 
 t.write("project-root.jam", "import gcc ;")
 t.write("Jamfile", "lib a : a.cpp : <include>. ;")
-t.write("a.cpp", "void foo() {}")
+t.write("a.cpp", """
+#include <a.h>
+void foo() {}
+""")
 t.write("a.h", "")
 
 t.write("d/Jamfile", "exe b : b.cpp ../a ; ")
