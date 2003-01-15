3099d7337f3ff951f4e1880eac384df91ca3a6fe
Vladimir Prus <ghost@cs.msu.su>
2003-01-15 12:03:08 +0000

    Win32 fix.
    [SVN r16910]

--- test/stage.py
+++ test/stage.py
@@ -8,11 +8,11 @@ t = Tester()
 t.write("project-root.jam", "import gcc ;")
 t.write("Jamfile", """
 lib a : a.cpp ;
-stage dist : a a.h aux/1 ;
+stage dist : a a.h auxilliary/1 ;
 """)
 t.write("a.cpp", "")
 t.write("a.h", "")
-t.write("aux/1", "")
+t.write("auxilliary/1", "")
 
 t.run_build_system()
 t.expect_addition(["dist/a" + dll_suffix, "dist/a.h", "dist/1"])
