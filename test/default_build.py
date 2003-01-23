a193d9d287d049ad63451e56184c06713dc2f90d
Vladimir Prus <ghost@cs.msu.su>
2003-01-23 14:24:53 +0000

    Add (now disabled) test for BB10.
    [SVN r17008]

--- test/default_build.py
+++ test/default_build.py
@@ -13,4 +13,36 @@ t.run_build_system()
 t.expect_addition("bin/gcc/debug/a"+exe_suffix)
 t.expect_addition("bin/gcc/release/a"+exe_suffix)
 
+# Now try a harder example: default build which contains <define>
+# should cause <define> to be present when "b" is compiled.
+# This happens only of "build-project b" is placed first.
+t.write("Jamfile", """
+    project 
+	: default-build <define>FOO 
+	;
+
+    build-project a ;
+    build-project b ;	
+""")
+
+t.write("a/Jamfile", """
+    exe a : a.cpp ../b/b ;
+""")
+t.write("a/a.cpp", """
+void foo();
+int main() { foo(); }
+""")
+
+t.write("b/Jamfile", """
+    lib b : b.cpp ;
+""")
+t.write("b/b.cpp", """
+#ifdef FOO
+void foo() {}
+#endif
+""")
+
+# Uncomment when BB10 is resolved.
+#t.run_build_system()
+
 t.cleanup()
