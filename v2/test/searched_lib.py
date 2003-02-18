cec1703a3bfb563bc33a36326269d26b38f93f28
Vladimir Prus <ghost@cs.msu.su>
2003-02-18 06:56:12 +0000

    Don't link to -lz and assume it's existence: build a private library for testing. Also, test both <name> and <search> in one test.
    [SVN r17503]

--- v2/test/searched_lib.py
+++ v2/test/searched_lib.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 
 # Test usage of searched-libs: one which are found via -l
-# switch to the linker/compiler. This tests works with gcc
-# only, for the moment, and requires that libz is available.
+# switch to the linker/compiler. 
 
 from BoostBuild import Tester, exe_suffix
 import string
 t = Tester()
+
+# To start with, we have to prepate a library to link with
+t.write("lib/project-root.jam", "import gcc ; ")
+t.write("lib/Jamfile", "lib test_lib : test_lib.cpp ;")
+t.write("lib/test_lib.cpp", "void foo() {}\n");
+
+t.run_build_system(subdir="lib")
+t.expect_addition("lib/bin/gcc/debug/test_lib.so")
+
+t.copy("lib/bin/gcc/debug/test_lib.so", "lib/libtest_lib.so")
+
 
 # A regression test: <library>property referring to
 # searched-lib was mishandled. As the result, we were
@@ -25,20 +35,12 @@ int main() {}
 """)
 
 t.write('d/d2/Jamfile', """
-lib z : : <name>z ;
-lib a : a.cpp : : : <library>z ;
+lib test_lib : : <name>test_lib <search>../../lib ;
+lib a : a.cpp : : : <library>test_lib ;
 """)
 t.write('d/d2/a.cpp', """
 """)
 
 t.run_build_system()
-
-# Test the <search> properties is respected.
-t.write('Jamfile', """exe main : main.cpp z ;
-lib z : : <name>z <search>PATH_TO_ZLIB ;  
-""")
-
-t.run_build_system(extra_args='-a -n -d2')
-t.fail_test(string.find(t.stdout(), "PATH_TO_ZLIB") == -1)
 
 t.cleanup()
