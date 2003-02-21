e55c3bc7b607f24a9ba38b30130900b38a45ce78
Rene Rivera <grafikrobot@gmail.com>
2003-02-21 17:22:21 +0000

    Removed outdated build scripts.
    [SVN r17578]

--- src/engine/makecygwindebugjam.bat
+++
@@ -1,4 +0,0 @@
-set VISUALC=c:\tools\msvc6\vc98
-set JAM_TOOLSET=VISUALC
-rm -rf bin.ntx86
-make -fbuilds/win32-gcc.mk CFLAGS="/GZ /Zi /MLd -DNT" CCFLAGS="/GZ /Zi /MLd" YACC="bison -t -d -l -v --yacc" LINKLIBS="c:\tools\msvc6\vc98\lib\advapi32.lib c:\tools\msvc6\vc98\lib\oldnames.lib c:\tools\msvc6\vc98\lib\gdi32.lib c:\tools\msvc6\vc98\lib\user32.lib c:\tools\msvc6\vc98\lib\kernel32.lib" LINKFLAGS="/DEBUG" %*
--- src/engine/makedebugjam.bat
+++
@@ -1,8 +0,0 @@
-call c:\tools\msvc6\vc98\bin\vcvars32.bat
-set VISUALC=c:\tools\msvc6\vc98
-set JAM_TOOLSET=VISUALC
-rm -rf bin.ntx86/jam.exe bin.ntx86/*.obj bin.ntx86/*.lib
-set BOOST_ROOT=
-set BOOST_BUILD_PATH=
-set JAMBASE=
-nmake -fbuilds/win32-visualc.mk JAMBASE= BOOST_ROOT= BOOST_BUILD_PATH= CFLAGS="/GZ /Zi /MLd -DNT -DYYDEBUG" CCFLAGS="/GZ /Zi /MLd -DYYDEBUG" LINKLIBS="c:\tools\msvc6\vc98\lib\advapi32.lib c:\tools\msvc6\vc98\lib\oldnames.lib c:\tools\msvc6\vc98\lib\gdi32.lib c:\tools\msvc6\vc98\lib\user32.lib c:\tools\msvc6\vc98\lib\kernel32.lib" LINKFLAGS="/DEBUG" YACC="byacc" YACCFILES="y.tab" YACCFLAGS=-vd %*
