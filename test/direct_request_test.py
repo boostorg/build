ae90c56249430352b7af3db6b1b4e3968ac28b57
Vladimir Prus <ghost@cs.msu.su>
2003-02-27 08:51:37 +0000

    Bugfix: nested 'build-project' statements were not handled correctly when any build request is given on command line. Thanks to Jürgen Hunold for catching this!
    [SVN r17669]

--- test/direct_request_test.py
+++ test/direct_request_test.py
@@ -12,6 +12,17 @@ t.run_build_system(extra_args="define=MACROS")
 
 t.expect_addition("bin/$toolset/debug/" 
                   * (List("a.o b.o b.dll a.exe")))
+		  
+# Regression test: direct build request was not working
+# when there's more than one level of 'build-project'
+
+t.rm(".")
+t.write('project-root.jam', '')
+t.write('Jamfile', 'build-project a ;')
+t.write('a/Jamfile', 'build-project b ;')
+t.write('a/b/Jamfile', '')
+
+t.run_build_system("release")		  
 
 
 t.cleanup()
