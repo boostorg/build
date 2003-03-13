c3daa69e05089f721d78e78d379488571b7040f0
Vladimir Prus <ghost@cs.msu.su>
2003-03-13 10:09:06 +0000

    Bugfix: translate path properties even in conditional properties.
    * new/property.jam
      (split-conditional): New rule
      (translate-paths): Use the above to handle conditional properties.
    
    * test/path_features.py: New tests.
    
    
    [SVN r17881]

--- test/path_features.py
+++ test/path_features.py
@@ -23,4 +23,20 @@ t.write("d/b.cpp", """
 
 t.run_build_system(subdir="d")
 
+# Now test the path features with condition work as well
+t.write("Jamfile", "lib a : a.cpp : <variant>debug:<include>.  ;")
+t.rm("bin")
+t.run_build_system(subdir="d")
+
+# Test path features with condtion in usage requirements
+t.write("Jamfile", "lib a : a.cpp : <include>. : : <variant>debug:<include>. ;")
+t.write("d/b.cpp", """
+#include <a.h>
+void foo();
+int main() { foo(); }
+""")
+t.rm("d/bin")
+t.run_build_system(subdir="d")
+
+
 t.cleanup()
