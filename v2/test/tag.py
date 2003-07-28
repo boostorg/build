67164c95315c9ac5ac6c193f0251ef1c81e10c7b
Vladimir Prus <ghost@cs.msu.su>
2003-07-28 08:25:29 +0000

    Correct the copyright.
    [SVN r19325]

--- v2/test/tag.py
+++ v2/test/tag.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2003. Permission to copy, use, modify, sell and
+#  Copyright (C) Pedro Ferreira 2003. Permission to copy, use, modify, sell and
 #  distribute this software is granted provided this copyright notice appears in
 #  all copies. This software is provided "as is" without express or implied
 #  warranty, and with no claim as to its suitability for any purpose.
