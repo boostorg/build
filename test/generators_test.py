d02a14907db5dfc4d0d7a8afbad52122a51f1785
Vladimir Prus <ghost@cs.msu.su>
2003-09-10 07:39:45 +0000

    Bugfix: allow a generator to create two targets of the same type.
    * new/generators.jam
      (generator.__init__): Use parallel lists to store prefixes and postfixes
      for a name, not mapping with type as a key, since type is not unique.
      (generator.generated-targets): Adjust.
    
    
    [SVN r19990]

--- test/generators_test.py
+++ test/generators_test.py
@@ -13,7 +13,7 @@ t.expect_addition(
     * (
     List(   
        "a.obj b.obj c.h c.cpp c.obj d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp "
-        + "d_parser.lr0 x.c x.obj y.x1 y.x2 "
+        + "d_parser.lr0 d_parser.h d_parser_symbols.h x.c x.obj y.x1 y.x2 "
         + "y.cpp y.obj e.marked.cpp e.positions e.target.cpp e.obj "
         + "a.exe e.exe"))
     )
