c16af478628e2b619a2855c7776a9ef877865e96
Vladimir Prus <ghost@cs.msu.su>
2003-10-08 05:40:16 +0000

    Bugfix: path-constant rule was mishandling absolute paths on Windows.
    [SVN r20297]

--- test/project_root_constants.py
+++ test/project_root_constants.py
@@ -22,4 +22,22 @@ t.write("Jamfile", """
 t.run_build_system()
 t.fail_test(find(t.stdout(), "foobar") == -1)
 
+# Regression test: when absolute paths were passed to path-constant rule,
+# Boost.Build failed to recognize path as absolute and prepended current dir.
+t.write("project-root.jam", """
+import path ;
+local here = [ path.native [ path.pwd ] ] ;
+path-constant HERE : $(here) ;
+if $(HERE) != $(here) 
+{
+    ECHO "PWD           =" $(here) ;
+    ECHO "path constant =" $(HERE) ;
+    EXIT ;
+}
+""")
+t.write("Jamfile", "")
+
+t.run_build_system()
+
+
 t.cleanup()
