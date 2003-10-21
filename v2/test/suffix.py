65f15750103a542c3e34d18f3bf9845a3d9991dd
Vladimir Prus <ghost@cs.msu.su>
2003-10-21 07:24:28 +0000

    Bugfix. It was not possible to specify empty suffix to a type if base type had nonempty suffix.
    [SVN r20435]

--- v2/test/suffix.py
+++ v2/test/suffix.py
@@ -5,13 +5,12 @@
 #  all copies. This software is provided "as is" without express or implied
 #  warranty, and with no claim as to its suitability for any purpose.
 
-#  Regression test: when staging V2 used to change suffixes on targets
-#  corresponding to real files.
-
 from BoostBuild import Tester, List
 
 t = Tester()
 
+#  Regression test: when staging V2 used to change suffixes on targets
+#  corresponding to real files.
 t.write("Jamfile", """ 
 import type : register ;
 register A : a1 a2 a3 ;
@@ -25,5 +24,42 @@ t.write("a.a3", "")
 t.run_build_system()
 t.expect_addition("a/a.a3");
 
-t.cleanup()
+# Regression test: we should be able to specify empty suffix for
+# derived target type, even if base type has non-empty suffix.
+t.write("a.cpp", "")
 
+t.write("suffixes.jam", """ 
+import type ;
+import generators ;
+import common ;
+
+type.register First : first : ;
+type.register Second : "" : First : main ;
+
+generators.register-standard $(__name__).second : CPP : Second ;
+
+rule second
+{
+    TOUCH on $(<) = [ common.file-creation-command ] ;
+}
+
+actions second
+{
+    $(TOUCH) $(<)
+}
+
+""")
+
+t.write("project-root.jam", """ 
+import suffixes ; 
+""")
+
+t.write("Jamfile", """ 
+second a : a.cpp ; 
+""")
+
+t.run_build_system()
+t.expect_addition("bin/$toolset/debug/a")
+
+
+t.cleanup()
