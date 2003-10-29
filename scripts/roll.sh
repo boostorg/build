f9b7715de2f86c9f14eaeacd2b5e1cb84c65c12e
Vladimir Prus <ghost@cs.msu.su>
2003-10-29 10:48:00 +0000

    Update release procedure.
    [SVN r20538]

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Grab jam_src
-cp -r ../jam_src .
+# Do some renames/rearrangments
 
-# Clean up
-rm -rf examples
+cp -r ../v2 ../boost-build
+# Grab jam_src
+cp -r ../jam_src ../boost-build
+cd ../boost-build
+
 # This one is not fully finished
-rm -rf examples-v2/versioned
+rm -rf example/versioned
 # This one is too low-level and misleading
-rm -rf examples-v2/make
+rm -rf example/make
 
 find . -maxdepth 1 -type f | egrep -v "roll.sh|build-system.jam|boost_build_v2.html|c++boost.gif|index_v2.html|hacking.txt" | xargs rm -f
 
@@ -25,4 +27,4 @@ cd boost-build
 
 # Upload docs to sourceforge
 perl -pi -e 's%<!-- sf logo -->%<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7586&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" align="right"/></a>%' boost_build_v2.html index.html doc/*.html
-scp -r doc examples-v2 *.html hacking.txt vladimir_prus@shell.sourceforge.net:/home/groups/b/bo/boost/htdocs/boost-build2
+scp -r doc example *.html hacking.txt vladimir_prus@shell.sourceforge.net:/home/groups/b/bo/boost/htdocs/boost-build2
