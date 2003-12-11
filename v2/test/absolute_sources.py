8184b26a6fda35633aa9932ad9437811107b5d39
Vladimir Prus <ghost@cs.msu.su>
2003-12-11 13:46:11 +0000

    Fix a couple of bugs with absolute filesnames.
    [SVN r21225]

--- v2/test/absolute_sources.py
+++ v2/test/absolute_sources.py
@@ -19,4 +19,37 @@ t.write("empty.cpp", "\n")
 t.run_build_system()
 t.expect_addition("bin/$toolset/debug/hello.exe")
 
+# Test a contrived case. There, absolute name is used in
+# standalone project (not Jamfile). Moreover, the target with
+# absolute name is returned by 'alias' and used from other project.
+t.write("a.cpp", """ 
+int main()
+{
+    return 0;
+}
+
+""")
+
+t.write("Jamfile", """ 
+exe a : /standalone//a ; 
+""")
+
+t.write("project-root.jam", """ 
+import standalone ; 
+""")
+
+t.write("standalone.jam", """ 
+import project ;
+
+project.initialize $(__name__) ;
+project standalone ;
+
+local pwd = [ PWD ] ;
+
+alias a : $(pwd)/a.cpp ;
+
+""")
+t.run_build_system()
+t.expect_addition("bin/$toolset/debug/a.exe")
+
 t.cleanup()
