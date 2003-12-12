832bb396dbb255a387cbd2a622bd45fcdfb05aa2
Dave Abrahams <dave@boostpro.com>
2003-12-12 16:26:23 +0000

    Fix up C++-isms in C code.
    [SVN r21239]

--- src/engine/native.c
+++ src/engine/native.c
@@ -14,18 +14,21 @@ void declare_native_rule(char* module, char* rule, char** args,
     if (m->native_rules == 0) {
         m->native_rules = hashinit( sizeof( native_rule_t ), "native rules");
     }
-    native_rule_t n, *np = &n;
-    n.name = rule;
-    if (args)
+    
     {
-        n.arguments = args_new();
-        lol_build( n.arguments->data, args );
-    }   
-    else
-    {
-        n.arguments = 0;
+        native_rule_t n, *np = &n;
+        n.name = rule;
+        if (args)
+        {
+            n.arguments = args_new();
+            lol_build( n.arguments->data, args );
+        }   
+        else
+        {
+            n.arguments = 0;
+        }
+        n.procedure = parse_make( f, P0, P0, P0, C0, C0, 0 );        
+        hashenter(m->native_rules, (HASHDATA**)&np);
     }
-    n.procedure = parse_make( f, P0, P0, P0, C0, C0, 0 );        
-    hashenter(m->native_rules, (HASHDATA**)&np);
 }
 
