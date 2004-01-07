5fc725707e9419e9e35f2fde2504b13369333ba5
Dave Abrahams <dave@boostpro.com>
2004-01-07 03:47:11 +0000

    Fixed bugs
    [SVN r21521]

--- jam_src/w32_getreg.c
+++ jam_src/w32_getreg.c
@@ -3,19 +3,20 @@
 # include "newstr.h"
 # include "parse.h"
 # include "frames.h"
+# include "strings.h"
 
 # define WIN32_LEAN_AND_MEAN
 # include <windows.h>
 
 # define  MAX_REGISTRY_DATA_LENGTH  4096
 
-typedef struct _KeyMap
+typedef struct
 {
-    LPCSTR  szName;
-    HKEY    hValue;
+    LPCSTR  name;
+    HKEY    value;
 } KeyMap;
 
-static const KeyMap  dlRootKeys[] = {
+static const KeyMap dlRootKeys[] = {
     { "HKLM", HKEY_LOCAL_MACHINE },
     { "HKCU", HKEY_CURRENT_USER },
     { "HKCR", HKEY_CLASSES_ROOT },
@@ -30,64 +31,88 @@ builtin_system_registry(
     PARSE    *parse,
     FRAME    *frame )
 {
-    HKEY   hKey, hRootKey = 0;
-    LIST   *dlResult = L0;
-    LPSTR  szKey;
-
+    char const* path = lol_get(frame->args, 0)->string;
+    LIST* result = L0;
+    HKEY key;
+    
     {
-    const KeyMap  *pdSearchKey = dlRootKeys;
-    LPSTR  szRootKey = newstr( lol_get(frame->args, 0)->string );
-
-    szKey = strchr(szRootKey , '\\');
-    *szKey = '\0'; ++szKey;
-
-    do {
-        if ( !strcmp(szRootKey, pdSearchKey->szName) )
-        { hRootKey = pdSearchKey->hValue; break; }
-        else  ++pdSearchKey;
-    } while (pdSearchKey->szName);
-    }
-
-    if ( hRootKey && ERROR_SUCCESS ==
-         RegOpenKeyEx(hRootKey, szKey, 0, KEY_QUERY_VALUE, &hKey) )
-    {
-        DWORD  dwType, dwSize;
-        BYTE   bsDataValue[MAX_REGISTRY_DATA_LENGTH];
-        LPSTR  szKeyValue;
-        LIST  *lKeyValue = lol_get(frame->args, 1);
+        const KeyMap *p;
         
-        if (lKeyValue)  szKeyValue = lKeyValue->string;
-        else  szKeyValue = NULL;
-
-        if ( ERROR_SUCCESS ==
-             RegQueryValueEx(hKey, szKeyValue, 0, &dwType, bsDataValue, &dwSize) )
+        for (p = dlRootKeys; p->name; ++p)
         {
-            switch (dwType)
+            int n = strlen(p->name);
+            if (!strncmp(path,p->name,n))
             {
-            case REG_EXPAND_SZ:
-                 {
-                 CHAR  szResult[MAX_REGISTRY_DATA_LENGTH];
-                 ExpandEnvironmentStrings( (LPCSTR)bsDataValue,
-                     szResult, MAX_REGISTRY_DATA_LENGTH );
-
-                 dlResult = list_new( dlResult, newstr(szResult) ); break;
-                 }
-            case REG_MULTI_SZ:
-                 {
-                 LPSTR  pszString = (LPSTR)bsDataValue;
-                 while (*pszString)
-                 {
-                     dlResult = list_new( dlResult, newstr(pszString) );
-                     pszString = pszString + strlen(pszString);
-                     ++pszString;
-                 }
-                 break;
-                 }
-            case REG_DWORD: sprintf( bsDataValue, "%u", *(PDWORD)bsDataValue );
-            default: dlResult = list_new( dlResult, newstr(bsDataValue) ); break;
+                if (path[n] == '\\' || path[n] == 0)
+                {
+                    path += n + 1;
+                    break;
+                }
             }
         }
-        RegCloseKey(hKey);
+        
+        key = p->value;
     }
-    return  dlResult;
+
+    if (
+        key != 0
+        && ERROR_SUCCESS == RegOpenKeyEx(key, path, 0, KEY_QUERY_VALUE, &key) 
+    )
+    {
+        DWORD  type, len;
+        BYTE   data[MAX_REGISTRY_DATA_LENGTH];
+        LIST const* const field = lol_get(frame->args, 1);
+        
+        if ( ERROR_SUCCESS ==
+             RegQueryValueEx(key, field ? field->string : 0, 0, &type, data, &len) )
+        {
+            switch (type)
+            {
+                
+             case REG_EXPAND_SZ:
+                 {
+                     long len;
+                     string expanded[1];
+                     string_new(expanded);
+
+                     while (
+                         (len = ExpandEnvironmentStrings(
+                             (LPCSTR)data, expanded->value, expanded->capacity))
+                         > expanded->capacity
+                     )
+                         string_reserve(expanded, len);
+
+                     expanded->size = len - 1;
+
+                     result = list_new( result, newstr(expanded->value) );
+                     string_free( expanded );
+                 }
+                 break;
+            
+             case REG_MULTI_SZ:
+                 {
+                     char* s;
+
+                     for (s = (char*)data; *s; s += strlen(s) + 1)
+                         result = list_new( result, newstr(s) );
+
+                 }
+                 break;
+             
+             case REG_DWORD:
+                 {
+                     char buf[100];
+                     sprintf( buf, "%u", *(PDWORD)data );
+                     result = list_new( result, newstr(buf) );
+                 }
+                 break;
+
+             case REG_SZ:
+                 result = list_new( result, newstr((char*)data) );
+                 break;
+            }
+        }
+        RegCloseKey(key);
+    }
+    return  result;
 }
