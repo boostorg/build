b11dfc449008055e5c22b00f0eacc545c289a43b
Rene Rivera <grafikrobot@gmail.com>
2004-07-26 04:46:01 +0000

    BSL.
    [SVN r24062]

--- v1/example/lib_use/simple_lib.cpp
+++ v1/example/lib_use/simple_lib.cpp
@@ -1,6 +1,6 @@
 /* (C) Copyright Rene Rivera, 2002.
-**
-** See accompanying license for terms and conditions of use.
+** Distributed under the Boost Software License, Version 1.0.
+** (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 */
 
 extern int lib_call(int x);
