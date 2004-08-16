82eeb79c31321e3e07b6821f3ff75f7c26c75322
Dave Abrahams <dave@boostpro.com>
2004-08-16 14:44:19 +0000

    fix copyright/license
    [SVN r24509]

--- v2/test/v1-testing/a.cpp
+++ v2/test/v1-testing/a.cpp
@@ -1,11 +1,5 @@
-//  Copyright (c) 2003 David Abrahams
-//
-//  Permission to copy, use, modify, sell and distribute this software
-//  is granted provided this copyright notice appears in all copies.
-//  This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
-//
-//  http://www.boost.org
-// 
+// Copyright David Abrahams 2003-2004. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 void foo() {}
