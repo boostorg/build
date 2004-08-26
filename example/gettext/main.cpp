82d9d4256bd38d5dd557bc57fca313ae209f6a01
Vladimir Prus <ghost@cs.msu.su>
2004-08-26 11:05:22 +0000

    Add copyright
    [SVN r24746]

--- example/gettext/main.cpp
+++ example/gettext/main.cpp
@@ -1,3 +1,8 @@
+// Copyright Vladimir Prus 2003.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 
 #include <locale.h>
 #include <libintl.h>
