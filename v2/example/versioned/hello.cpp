70006ad126562419b32dd009d422302c19574fbd
Vladimir Prus <ghost@cs.msu.su>
2004-08-26 11:19:23 +0000

    BSL
    [SVN r24749]

--- v2/example/versioned/hello.cpp
+++ v2/example/versioned/hello.cpp
@@ -1,7 +1,7 @@
-/* (C) Copyright Rene Rivera, 2003.
-**
-** See accompanying license for terms and conditions of use.
-*/
+// Copyright Rene Rivera, 2003
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
 
