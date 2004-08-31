3b0b49a6fca49581a938bd2542b18903256c1498
Vladimir Prus <ghost@cs.msu.su>
2004-08-31 06:58:52 +0000

    Write a new example to avoid lengthy "what is the license" and "how to refer to the QPL" and "is it OK to have QPL example" discussions.
    [SVN r24834]

--- example/qt/main.cpp
+++ example/qt/main.cpp
@@ -1,55 +1,36 @@
-/****************************************************************
-**
-** Qt tutorial 7
-**
-****************************************************************/
+// Copyright Vladimir Prus 2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include "canvas.h"
 #include <qapplication.h>
-#include <qpushbutton.h>
-#include <qlcdnumber.h>
-#include <qfont.h>
 #include <qvbox.h>
-#include <qgrid.h>
+#include <qpushbutton.h>
 
-#include "lcdrange.h"
-
-
-class MyWidget : public QVBox
+class Window : public QMainWindow
 {
 public:
-    MyWidget( QWidget *parent=0, const char *name=0 );
+    Window()
+    {
+        setCaption("QCanvas test");
+        QVBox* vb = new QVBox(this);
+        setCentralWidget(vb);   
+    
+        Canvas* c = new Canvas(vb); 
+        QPushButton* b = new QPushButton("Change color", vb);
+        connect(b, SIGNAL(clicked()), c, SLOT(change_color())); 
+    }        
 };
 
-
-MyWidget::MyWidget( QWidget *parent, const char *name )
-        : QVBox( parent, name )
+int main(int argc, char **argv)
 {
-    QPushButton *quit = new QPushButton( "Quit", this, "quit" );
-    quit->setFont( QFont( "Times", 18, QFont::Bold ) );
+    QApplication app(argc, argv);
+    Window *w = new Window();
 
-    connect( quit, SIGNAL(clicked()), qApp, SLOT(quit()) );
-
-    QGrid *grid = new QGrid( 4, this );
-
-    LCDRange *previous = 0;
-    for( int r = 0 ; r < 4 ; r++ ) {
-    for( int c = 0 ; c < 4 ; c++ ) {
-        LCDRange* lr = new LCDRange( grid );
-        if ( previous )
-        connect( lr, SIGNAL(valueChanged(int)),
-             previous, SLOT(setValue(int)) );
-        previous = lr;
-    }
-    }
+    app.setMainWidget(w);
+    w->show();
+    
+    return app.exec();
 }
 
-
-int main( int argc, char **argv )
-{
-    QApplication a( argc, argv );
-
-    MyWidget w;
-    a.setMainWidget( &w );
-    w.show();
-    return a.exec();
-}
