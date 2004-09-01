3accad8f3ad2fa7e96e54c7a1c7876acb0d2240b
Vladimir Prus <ghost@cs.msu.su>
2004-09-01 06:37:24 +0000

    BSL
    [SVN r24843]

--- v2/example/qt-ui/main.cpp
+++ v2/example/qt-ui/main.cpp
@@ -1,3 +1,8 @@
+// Copyright Felix E. Klee, 2003
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include "hello_world_widget.h"
 #include <qapplication.h>
 
