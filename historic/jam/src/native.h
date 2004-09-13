718f8e4eb339bb021cb28e39002eadde74c484e7
Vladimir Prus <ghost@cs.msu.su>
2004-09-13 16:19:19 +0000

    BSL
    [SVN r25055]

--- historic/jam/src/native.h
+++ historic/jam/src/native.h
@@ -1,3 +1,6 @@
+/* Copyright David Abrahams 2003. Distributed under the Boost */
+/* Software License, Version 1.0. (See accompanying */
+/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
 
 #ifndef NATIVE_H_VP_2003_12_09
 #define NATIVE_H_VP_2003_12_09
