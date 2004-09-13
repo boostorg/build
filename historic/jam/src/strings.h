5809be7f52160ada9dae7a5dfcb4a239acedcbb9
Dave Abrahams <dave@boostpro.com>
2004-09-13 15:46:44 +0000

    copyright-license
    [SVN r25050]

--- historic/jam/src/strings.h
+++ historic/jam/src/strings.h
@@ -1,6 +1,10 @@
 #ifndef STRINGS_DWA20011024_H
 # define STRINGS_DWA20011024_H
 
+/* Copyright David Abrahams 2004. Distributed under the Boost */
+/* Software License, Version 1.0. (See accompanying */
+/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
 # include <stddef.h>
 
 typedef struct string
