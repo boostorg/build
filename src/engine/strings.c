06ca8b669de45bee17283e50564f0b31e9de32cc
Dave Abrahams <dave@boostpro.com>
2004-09-13 15:46:44 +0000

    copyright-license
    [SVN r25050]

--- src/engine/strings.c
+++ src/engine/strings.c
@@ -1,4 +1,8 @@
-#include "strings.h"
+/* Copyright David Abrahams 2004. Distributed under the Boost */
+/* Software License, Version 1.0. (See accompanying */
+/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
+#Include "strings.h"
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
