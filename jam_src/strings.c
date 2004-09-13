54d611179508c0d9a7a543c2fb86dd5ed5d2673b
Martin Wille <mw8329@yahoo.com.au>
2004-09-13 21:55:37 +0000

    -- typo
    [SVN r25061]

--- jam_src/strings.c
+++ jam_src/strings.c
@@ -2,7 +2,7 @@
 /* Software License, Version 1.0. (See accompanying */
 /* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
 
-#Include "strings.h"
+#include "strings.h"
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
