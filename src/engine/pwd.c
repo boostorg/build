0a31772f1c19de4d58867c41e5ba2f61f75a64f8
Vladimir Prus <ghost@cs.msu.su>
2004-09-13 16:19:19 +0000

    BSL
    [SVN r25055]

--- src/engine/pwd.c
+++ src/engine/pwd.c
@@ -1,3 +1,7 @@
+/* Copyright Vladimir Prus 2002. Distributed under the Boost */
+/* Software License, Version 1.0. (See accompanying */
+/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
 #include "jam.h"
 #include "lists.h"
 #include "newstr.h"
