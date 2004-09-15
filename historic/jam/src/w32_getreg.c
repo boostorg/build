ac6399f4c0a46ab304787b6008ce22775b6cda70
John Maddock <john@johnmaddock.co.uk>
2004-09-15 11:06:07 +0000

    Fixed missing comment end
    [SVN r25108]

--- historic/jam/src/w32_getreg.c
+++ historic/jam/src/w32_getreg.c
@@ -1,6 +1,6 @@
 /* Copyright Paul Lin 2003. Distributed under the Boost */
 /* Software License, Version 1.0. (See accompanying */
-/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
 
 # include "jam.h"
 # include "lists.h"
@@ -120,3 +120,4 @@ builtin_system_registry(
     }
     return  result;
 }
+
