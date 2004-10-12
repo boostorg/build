e57fd7f06276c100ab70ad9626efe4dfcfebbf49
Vladimir Prus <ghost@cs.msu.su>
2004-10-12 08:28:16 +0000

    Don't remove index.html when making packages
    [SVN r25685]

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -13,7 +13,7 @@ rm -rf example/versioned
 rm -rf example/make
 
 # Remove unnecessary top-level files
-find . -maxdepth 1 -type f | egrep -v "roll.sh|bootstrap.jam|build-system.jam|boost_build_v2.html|boost.png|index_v2.html|hacking.txt|site-config.jam|user-config.jam" | xargs rm -f
+find . -maxdepth 1 -type f | egrep -v "roll.sh|bootstrap.jam|build-system.jam|boost_build_v2.html|boost.png|index.html|hacking.txt|site-config.jam|user-config.jam" | xargs rm -f
 
 # Prepare some more files.
 echo -e "boost-build kernel ;\n" > boost-build.jam
@@ -38,5 +38,5 @@ cd .. && zip -r boost-build.zip boost-build && tar --bzip2 -cf boost-build.tar.b
 cd boost-build
 
 # Upload docs to sourceforge
-perl -pi -e 's%<!-- sf logo -->%<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7586&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" align="right"/></a>%' boost_build_v2.html index.html doc/*.html
+perl -pi -e 's%<!-- sf logo -->%<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7586&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" align="right"/></a>%' index.html doc/*.html
 scp -r doc example *.html hacking.txt vladimir_prus@shell.sourceforge.net:/home/groups/b/bo/boost/htdocs/boost-build2
