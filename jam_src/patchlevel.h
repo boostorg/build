b5b4145076d477bc87b6903ef116315eb09b34bb
John Maddock <john@johnmaddock.co.uk>
2004-10-27 11:45:36 +0000

    Added original jam copyrights.
    [SVN r25888]

--- jam_src/patchlevel.h
+++ jam_src/patchlevel.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright 1993-2002 Christopher Seiwald and Perforce Software, Inc.
+ *
+ * This file is part of Jam - see jam.c for Copyright information.
+ */
+
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
