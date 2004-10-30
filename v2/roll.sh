b7667ddf3e989e9745e60d0699db141f8e3cba5f
Vladimir Prus <ghost@cs.msu.su>
2004-10-30 09:42:37 +0000

    Merge BBV2 Milestone_10 to RC_1_32_0
    [SVN r25974]

--- v2/roll.sh
+++ v2/roll.sh
@@ -23,10 +23,16 @@ touch doc/project-root.jam
 export BOOST_ROOT=/home/ghost/Work/boost-rc
 cd doc
 bjam --v2
+bjam --v2 pdf
+cp `find bin -name "*.pdf"` ../..
 rm -rf bin
 cd ..
 
+# Get the boost logo.
+wget http://boost.org/boost-build2/boost.png
+
 # Adjust the links, so they work with the standalone package
+perl -pi -e 's%../../../boost.png%boost.png%' index.html
 perl -pi -e 's%../../../doc/html/bbv2.html%doc/html/index.html%' index.html
 perl -pi -e 's%../../../doc/html/bbv2.installation.html%doc/html/bbv2.installation.html%' index.html
 
@@ -37,6 +43,7 @@ chmod a+x jam_src/build.bat
 cd .. && zip -r boost-build.zip boost-build && tar --bzip2 -cf boost-build.tar.bz2 boost-build
 cd boost-build
 
+chmod -R u+w *
 # Upload docs to sourceforge
 perl -pi -e 's%<!-- sf logo -->%<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7586&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" align="right"/></a>%' index.html doc/*.html
-scp -r doc example *.html hacking.txt vladimir_prus@shell.sourceforge.net:/home/groups/b/bo/boost/htdocs/boost-build2
+scp -r ../userman.pdf doc example *.html hacking.txt vladimir_prus@shell.sourceforge.net:/home/groups/b/bo/boost/htdocs/boost-build2
