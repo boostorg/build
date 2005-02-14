97e91c8e3f0ea80c8174e44edc0768fd47d69aac
Vladimir Prus <ghost@cs.msu.su>
2005-02-14 08:25:07 +0000

    Tweak computation of target names from source names.
    * build/generators.jam
      (generator.generated-targets): Comment the desired semantics.
    
    * util/utility.jam
      (basename): Take the part till the last dot, not the first.
    
    
    [SVN r27374]

--- test/generators_test.py
+++ test/generators_test.py
@@ -14,7 +14,7 @@ t.expect_addition(
     List(   
        "a.obj b.obj c.h c.cpp c.obj d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp "
         + "d_parser.lr0 d_parser.h d_parser_symbols.h x.c x.obj y.x1 y.x2 "
-        + "y.cpp y.obj e.marked.cpp e.positions e.target.cpp e.obj "))
+        + "y.cpp y.obj e.marked_cpp e.positions e.target_cpp e.obj "))
     )
 ok = 0
 
