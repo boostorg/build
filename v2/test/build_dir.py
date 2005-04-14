1b04aca862668f895779d1d389cadbed66c088c0
Vladimir Prus <ghost@cs.msu.su>
2005-04-14 10:34:11 +0000

    Implemented the --build-dir option.
    [SVN r28236]

--- v2/test/build_dir.py
+++ v2/test/build_dir.py
@@ -4,6 +4,9 @@
 # the 'build-dir' project attribute.
 
 from BoostBuild import Tester
+import string
+import os
+
 t = Tester()
 
 
@@ -47,5 +50,50 @@ exe b : b.cpp ;
 t.run_build_system()
 t.expect_addition(["bin/$toolset/debug/a.exe",
                    "src/build/$toolset/debug/b.exe"])
-           
+
+# Now test the '--build-dir' option.
+t.rm(".")
+t.write("Jamroot", "")
+
+# Test that we get an error when no project id is specified.
+t.run_build_system("--build-dir=foo")
+t.fail_test(string.find(t.stdout(),
+                   "warning: the --build-dir option will be ignored") == -1)
+
+t.write("Jamroot", """
+project foo ;
+exe a : a.cpp ;
+build-project sub ;
+""")
+t.write("a.cpp", "int main() { return 0; }\n")
+t.write("sub/Jamfile", "exe b : b.cpp ;\n")
+t.write("sub/b.cpp", "int main() { return 0; }\n")
+
+t.run_build_system("--build-dir=build")
+t.expect_addition(["build/foo/$toolset/debug/a.exe",
+                   "build/foo/sub/$toolset/debug/b.exe"])
+
+t.write("Jamroot", """
+project foo : build-dir bin.v2 ;
+exe a : a.cpp ;
+build-project sub ;
+""")
+
+t.run_build_system("--build-dir=build")
+t.expect_addition(["build/foo/bin.v2/$toolset/debug/a.exe",
+                   "build/foo/bin.v2/sub/$toolset/debug/b.exe"])
+
+t.write("Jamroot", """
+project foo : build-dir %s ;
+exe a : a.cpp ;
+build-project sub ;
+""" % os.getcwd())
+
+t.run_build_system("--build-dir=build", status=1)
+t.fail_test(string.find(t.stdout(),
+                   "Absolute directory specified via 'build-dir' project attribute") == -1)
+
+
+
+
 t.cleanup()        
