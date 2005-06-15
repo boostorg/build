1b9681a9ed18ff869d4c64a9862c8d0dbb4e4d68
Vladimir Prus <ghost@cs.msu.su>
2005-06-15 13:19:07 +0000

    Correct license text
    [SVN r29587]

--- test/template.py
+++ test/template.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2003. Permission to copy, use, modify, sell and
-#  distribute this software is granted provided this copyright notice appears in
-#  all copies. This software is provided "as is" without express or implied
-#  warranty, and with no claim as to its suitability for any purpose.
+#  Copyright (C) FILL SOMETHING HERE 2005.
+#  Distributed under the Boost Software License, Version 1.0. (See
+#  accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)
 
 #  This file is template for Boost.Build tests. It creates a simple
 #  project that builds one exe from one source, and checks that the exe
