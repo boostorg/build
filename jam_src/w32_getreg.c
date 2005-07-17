7442359c21b1eddf13d593b2c2ca20a456662167
Rene Rivera <grafikrobot@gmail.com>
2005-07-17 16:09:33 +0000

    Fix uninitialized in/out param for getting the registry value. Thanks to  Alexey Pakhunov for the patch.
    [SVN r30142]

--- jam_src/w32_getreg.c
+++ jam_src/w32_getreg.c
@@ -63,8 +63,9 @@ builtin_system_registry(
         && ERROR_SUCCESS == RegOpenKeyEx(key, path, 0, KEY_QUERY_VALUE, &key) 
     )
     {
-        DWORD  type, len;
+        DWORD  type;
         BYTE   data[MAX_REGISTRY_DATA_LENGTH];
+        DWORD  len = sizeof(data);
         LIST const* const field = lol_get(frame->args, 1);
         
         if ( ERROR_SUCCESS ==
