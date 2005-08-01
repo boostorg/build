2856b43ee6e0287c3ccb1172373490656ea71047
Rene Rivera <grafikrobot@gmail.com>
2005-08-01 14:14:43 +0000

    Remove non-platform sources from bootstrap build.
    [SVN r30361]

--- jam_src/build.sh
+++ jam_src/build.sh
@@ -188,8 +188,7 @@ echo "###"
 YYACC_SOURCES="yyacc.c"
 MKJAMBASE_SOURCES="mkjambase.c"
 BJAM_SOURCES="\
- command.c compile.c execnt.c execunix.c execvms.c expand.c\
- filent.c fileos2.c fileunix.c filevms.c glob.c hash.c\
+ command.c compile.c execunix.c expand.c fileunix.c glob.c hash.c\
  hdrmacro.c headers.c jam.c jambase.c jamgram.c lists.c make.c make1.c\
  newstr.c option.c parse.c pathunix.c pathvms.c regexp.c\
  rules.c scan.c search.c subst.c timestamp.c variable.c modules.c\
