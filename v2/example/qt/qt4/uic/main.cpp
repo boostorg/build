ec673c840f44db4e4d1a45a2ea84f973c7c726e7
Vladimir Prus <ghost@cs.msu.su>
2005-09-19 07:48:36 +0000

    Add the "ui_" prefix to the header the uic generates, to match qmake's behaviour.
    Thanks to Ryan Winter for the hint.
    
    
    [SVN r31033]

--- v2/example/qt/qt4/uic/main.cpp
+++ v2/example/qt/qt4/uic/main.cpp
@@ -3,7 +3,7 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include "hello_world_widget.h"
+#include "ui_hello_world_widget.h"
 #include <qapplication.h>
 #include <qwidget.h>
 
