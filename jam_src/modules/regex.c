07d836e1773f0a374344fd5149a9a603111dae04
Vladimir Prus <ghost@cs.msu.su>
2005-09-28 14:28:46 +0000

    Enable native version of regex.transform.
    [SVN r31141]

--- jam_src/modules/regex.c
+++ jam_src/modules/regex.c
@@ -10,15 +10,16 @@
 #include "../compile.h"
 
 /*
-rule transform ( list * : pattern )
+rule transform ( list * : pattern : indices * )
 {
+    indices ?= 1 ;
     local result ;
     for local e in $(list)
     {
         local m = [ MATCH $(pattern) : $(e) ] ;
         if $(m)
         {
-            result += $(m[1]) ;
+            result += $(m[$(indices)]) ;
         }        
     }
     return $(result) ;
@@ -28,30 +29,60 @@ LIST *regex_transform( PARSE *parse, FRAME *frame )
 {
     LIST* l = lol_get( frame->args, 0 );    
     LIST* pattern = lol_get( frame->args, 1 );    
+    LIST* indices_list = lol_get(frame->args, 2);
+    int* indices = 0;
+    int size;
+    int* p;
     LIST* result = 0;
 
     string buf[1];
     string_new(buf);
 
-
-    /* Result is cached and intentionally never freed */
+    if (indices_list)
     {
+        size = list_length(indices_list);
+        indices = (int*)malloc(size*sizeof(int));
+        for(p = indices; indices_list; indices_list = indices_list->next)
+        {
+            *p++ = atoi(indices_list->string);
+        }        
+    }
+    else 
+    {
+        size = 1;
+        indices = (int*)malloc(sizeof(int));
+        *indices = 1;
+    }
+
+    {
+        /* Result is cached and intentionally never freed */
         regexp *re = regex_compile( pattern->string );
 
         for(; l; l = l->next)
         {
             if( regexec( re, l->string ) )
             {
-                if (re->startp[1])
+                int i = 0;
+                for(; i < size; ++i)
                 {
-                    string_append_range( buf, re->startp[1], re->endp[1] );
-                    result = list_new( result, newstr( buf->value ) );                
-                    string_truncate( buf, 0 );
+                    int index = indices[i];
+                    /* Skip empty submatches. Not sure it's right in all cases,
+                       but surely is right for the case for which this routine
+                       is optimized -- header scanning.
+                    */
+                    if (re->startp[index] != re->endp[index])
+                    {
+                        string_append_range( buf, re->startp[index], re->endp[index] );
+                        result = list_new( result, newstr( buf->value ) ); 
+                        string_truncate( buf, 0 );
+                    }
                 }
             }
         }
         string_free( buf );
     }
+
+    free(indices);
     
     return result;
 }
@@ -59,7 +90,7 @@ LIST *regex_transform( PARSE *parse, FRAME *frame )
 void init_regex()
 {
     {
-        char* args[] = { "list", "*", ":", "pattern", 0 };
-        declare_native_rule("regex", "transform", args, regex_transform, 1);
+        char* args[] = { "list", "*", ":", "pattern", ":", "indices", "*", 0 };
+        declare_native_rule("regex", "transform", args, regex_transform, 2);
     }
 }
