71e5eb8e9ceadd04551c7236b8ef63ecfbc649fd
Vladimir Prus <ghost@cs.msu.su>
2006-02-21 08:30:55 +0000

    Adjust to new jam location
    [SVN r33056]

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -7,9 +7,9 @@
 set -e
 cd /tmp
 rm -rf boost-build
-cvs -d :ext:vladimir_prus@cvs.sourceforge.net:/cvsroot/boost co -P -d  boost-build boost/tools/build > /tmp/boost_build_checkout_log
+cvs -d :ext:vladimir_prus@cvs.sourceforge.net:/cvsroot/boost co -P -d  boost-build boost/tools > /tmp/boost_build_checkout_log
 mv /tmp/boost_build_checkout_log boost-build/checkout-log
-cd boost-build/v2
+cd boost-build/build/v2
 ./roll.sh > ../roll-log
 cd ..
 scp boost-build.zip boost-build.tar.bz2 vladimir_prus@shell.sf.net:/home/groups/b/bo/boost/htdocs/boost-build2 > scp-log
