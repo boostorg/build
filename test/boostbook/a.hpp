5c9451d7c7f3cec07ad04105357fffceac84e570
Vladimir Prus <ghost@cs.msu.su>
2006-04-11 06:02:22 +0000

    Merge from trunk.
    [SVN r33653]

--- test/boostbook/a.hpp
+++ test/boostbook/a.hpp
@@ -1,7 +1,12 @@
 
+// Seems like Boostbook does like classes outside of namespaces,
+// and won't generate anything for them.
+namespace boost {
+
 /// A class
 class A {
 public:
     /// A constructor
     A();    
 };
+}
