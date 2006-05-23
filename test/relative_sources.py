aafe2d6065864a5e1a39b32e389240738bc7a4e3
Vladimir Prus <ghost@cs.msu.su>
2006-05-23 14:21:58 +0000

    Try to preserve relative paths in sources.
    Based on a patch from Ryan Gallagher.
    
    
    [SVN r34062]

--- test/relative_sources.py
+++ test/relative_sources.py
@@ -5,10 +5,30 @@
 from BoostBuild import Tester
 t = Tester()
 
-t.write("project-root.jam", "import gcc ;")
-t.write("Jamfile", "exe a : src/a.cpp ;")
+# Test that relative path to source, 'src', is preserved.
+t.write("Jamroot", "exe a : src/a.cpp ;")
 t.write("src/a.cpp", "int main() { return 0; }\n")
 
 t.run_build_system()
+t.expect_addition("bin/$toolset/debug/src/a.obj")
+
+
+# Test that the relative path to source is preserved
+# when using 'glob'.
+t.rm("bin")
+t.write("Jamroot", "exe a : [ glob src/*.cpp ] ;")
+t.run_build_system()
+t.expect_addition("bin/$toolset/debug/src/a.obj")
+
+
+# Test that relative path with ".." is *not* added to
+# target path.
+t.rm(".")
+t.write("Jamroot", "")
+t.write("a.cpp", "int main() { return 0; }\n")
+t.write("build/Jamfile", "exe a : ../a.cpp ; ")
+t.run_build_system(subdir="build")
+t.expect_addition("build/bin/$toolset/debug/a.obj")
+
 
 t.cleanup()
