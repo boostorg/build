deac4d9335d637b325cba07e40e9e6023cfd10b3
Vladimir Prus <ghost@cs.msu.su>
2006-07-20 08:09:50 +0000

    Fix expectations.
    [SVN r34616]

--- test/boostbook.py
+++ test/boostbook.py
@@ -12,11 +12,11 @@ t = Tester()
 
 t.set_tree("boostbook")
 # For some reason, the messages are sent to stderr.
-t.run_build_system(stderr="""Writing A.html for refentry(A)
+t.run_build_system(stderr="""Writing boost/A.html for refentry(boost.A)
 Writing library/reference.html for section(library.reference)
 Writing index.html for chapter(library)
 Writing HTML.manifest
 """)
-t.expect_addition(["html/A.html", "html/index.html"])
+t.expect_addition(["html/boost/A.html", "html/index.html"])
 
 t.cleanup()
