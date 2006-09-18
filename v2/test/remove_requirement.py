a18d9df30cea416f96f60d410ca9718849f9ae8f
Vladimir Prus <ghost@cs.msu.su>
2006-09-18 19:56:19 +0000

    Test for 'cancel requirement' feature
    [SVN r35169]

---
+++ v2/test/remove_requirement.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+#  Copyright (C) Vladimir Prus 2006.
+#  Distributed under the Boost Software License, Version 1.0. (See
+#  accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)
+
+from BoostBuild import Tester, List
+import string
+
+t = Tester()
+
+t.write("Jamroot", """ 
+project : requirements <threading>multi <variant>debug:<link>static ;
+
+build-project sub ;
+build-project sub2 ;
+build-project sub3 ;
+build-project sub4 ; 
+""")
+
+t.write("sub/Jamfile", """ 
+exe hello : hello.cpp : -<threading>multi ; 
+""")
+
+t.write("sub/hello.cpp", """ 
+int main()
+{
+    return 0;
+}
+
+""")
+
+t.write("sub2/Jamfile", """ 
+project : requirements -<threading>multi ;
+exe hello : hello.cpp ; 
+""")
+
+t.write("sub2/hello.cpp", """ 
+int main()
+{
+    return 0;
+}
+
+""")
+
+t.write("sub3/hello.cpp", """ 
+int main()
+{
+    return 0;
+}
+
+""")
+
+t.write("sub3/Jamfile", """ 
+exe hello : hello.cpp : -<variant>debug:<link>static ; 
+""")
+
+t.write("sub4/hello.cpp", """ 
+int main()
+{
+    return 0;
+}
+
+""")
+
+t.write("sub4/Jamfile", """ 
+project : requirements -<variant>debug:<link>static ;
+exe hello : hello.cpp ; 
+""")
+
+
+
+t.run_build_system()
+
+t.expect_addition("sub/bin/$toolset/debug/link-static/hello.exe")
+t.expect_addition("sub2/bin/$toolset/debug/link-static/hello.exe")
+t.expect_addition("sub3/bin/$toolset/debug/threading-multi/hello.exe")
+t.expect_addition("sub4/bin/$toolset/debug/threading-multi/hello.exe")
+
+t.cleanup()
+
