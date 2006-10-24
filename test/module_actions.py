c05a3ed3039f33f499590222bf98f7459d18431c
Rene Rivera <grafikrobot@gmail.com>
2006-10-24 16:34:34 +0000

    (merge from head) Adjust to make test pass on Windows.
    [SVN r35724]

--- test/module_actions.py
+++ test/module_actions.py
@@ -11,15 +11,15 @@ t = Tester(pass_toolset=0)
 
 t.set_tree('module-actions')
 
-expected = r'''A.act t1:   X1-t1     
-B.act t1:   X1-t1   X2-B   
-act t1:   X1-t1   X2-global   X3-global 
-A.act t2:   X1-A   X2-t2   
-B.act t2:     X2-t2   
-act t2:   X1-global   X2-t2   X3-global 
-A.act t3:   X1-A     X3-t3 
-B.act t3:     X2-B   X3-t3 
-act t3:   X1-global   X2-global   X3-t3 
+expected = r'''A.act t1: X1-t1   
+B.act t1: X1-t1 X2-B  
+act t1: X1-t1 X2-global X3-global 
+A.act t2: X1-A X2-t2  
+B.act t2:  X2-t2  
+act t2: X1-global X2-t2 X3-global 
+A.act t3: X1-A  X3-t3 
+B.act t3:  X2-B X3-t3 
+act t3: X1-global X2-global X3-t3 
 '''
 
 # On Unixes, call to 'echo 1     2      3' produces '1 2 3' (note spacing)
