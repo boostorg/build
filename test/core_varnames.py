602b6d8b13affd234282f3767d0686b2f8d134f2
Rene Rivera <grafikrobot@gmail.com>
2006-11-05 18:13:42 +0000

    Add/update copyrights+license (with help of a shell script).
    [SVN r35856]

--- test/core_varnames.py
+++ test/core_varnames.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# Copyright 2003 Dave Abrahams 
+# Distributed under the Boost Software License, Version 1.0. 
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
+
 # This tests the core rule for enumerating the variable names in a module
 
 import BoostBuild
