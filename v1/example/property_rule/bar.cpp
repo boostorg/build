c29dbe26f58fcc1f942177f3c5a0bccb0fef6dd8
Rene Rivera <grafikrobot@gmail.com>
2006-11-06 17:10:46 +0000

    Remove obsolete Boost.Build v1 files.
    [SVN r35880]

--- v1/example/property_rule/bar.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright David Abrahams 2002.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
-#include <foo.hpp>
-int main() { foo(); return 0; }
