f1493bf1fa5968673733f338f25aca8309796eaf
Dave Abrahams <dave@boostpro.com>
2006-12-11 03:35:10 +0000

    Remove BBv1 for good
    [SVN r36321]

--- v1/example/property_rule/inc/foo.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright David Abrahams 2002.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
-#ifndef FOO_DWA2002712_HPP
-# define FOO_DWA2002712_HPP
-
-void foo();
-
-#endif // FOO_DWA2002712_HPP
