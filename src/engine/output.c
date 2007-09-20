fcd347ebde57fbe8b28284333e3b7f5d63afb936
K. Noel Belcourt <kbelco@sandia.gov>
2007-09-20 19:06:54 +0000

    Added diagnostic message to output.c to inform users when a process has timed out and been killed.
    Because timed out processes now emit a diagnostic, I
    had to update option_l.jam so we wouldn't break test.sh
    when it runs.
    
    Minor cleanup to execunix.c to remove unneeded code
    and to set the process exit status as returned from
    waitpid.  The exit status is used to identify timed
    out processes so we can emit a diagnostic to the user.
    
    
    
    [SVN r39423]

--- src/engine/output.c
+++ src/engine/output.c
@@ -53,6 +53,22 @@ void out_action(
     {
         fputs(command,globs.cmdout);
     }
+
+    switch (exit_reason)
+    {
+        case EXIT_OK:
+            break;
+        case EXIT_FAIL:
+            break;
+        case EXIT_TIMEOUT:
+        {
+            /* process expired, make user aware with explicit message */
+            fprintf(bjam_out, "%d second time limit exceeded\n", globs.timeout);
+            break;
+        }
+        default:
+          break;
+    }
     
     /* print out the command output, if requested */
     if (0 != out_data &&
