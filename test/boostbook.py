49e9219a6186b858530fbaf0584aee65bd81fcf6
Vladimir Prus <ghost@cs.msu.su>
2007-09-29 18:17:27 +0000

    Adjust boostbook test for boostbook changes. It seems that doxygen target now produces a target with different name, and it does not produce any target unless there's explicit dependency on it. I'm not sure I like the change, but anyway.
    [SVN r39605]

--- test/boostbook.py
+++ test/boostbook.py
@@ -5,17 +5,19 @@
 # (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
 
 from BoostBuild import Tester, List
+import string
 
 # Create a temporary working directory
 t = Tester()
 
 t.set_tree("boostbook")
 # For some reason, the messages are sent to stderr.
-t.run_build_system(stderr="""Writing boost/A.html for refentry(boost.A)
+t.run_build_system()
+t.fail_test(string.find(t.stdout(), """Writing boost/A.html for refentry(boost.A)
 Writing library/reference.html for section(library.reference)
 Writing index.html for chapter(library)
-Writing HTML.manifest
-""")
+Writing docs_HTML.manifest
+""") == -1)
 t.expect_addition(["html/boost/A.html", "html/index.html"])
 
 t.cleanup()
