e29345438ea9ccb9888eac8685359d60f83bbee7
Rene Rivera <grafikrobot@gmail.com>
2007-11-18 20:24:25 +0000

    Inspection report fixes.
    [SVN r41212]

--- v2/test/duplicate.py
+++ v2/test/duplicate.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2004. Permission to copy, use, modify, sell and
-#  distribute this software is granted provided this copyright notice appears in
-#  all copies. This software is provided "as is" without express or implied
-#  warranty, and with no claim as to its suitability for any purpose.
+# Copyright 2004 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 #  This test tries to stage the same file to the same location by *two*
 #  different stage rules, in two different projects. This is not exactly
