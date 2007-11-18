a42f466fd2603098a055d1ca6af0cfde4dfc0111
Rene Rivera <grafikrobot@gmail.com>
2007-11-18 20:24:25 +0000

    Inspection report fixes.
    [SVN r41212]

--- example/python_modules/python_helpers.py
+++ example/python_modules/python_helpers.py
@@ -1,3 +1,6 @@
+# Copyright 2006 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 # Declare a couple of functions called from Boost.Build
 #
@@ -10,6 +13,6 @@
 
 def test1(l):
     return ["foo", "bar"]
-    
+
 def test2(l, l2):
     return [l[0], l2[0]]
--- test/dependency_test.py
+++ test/dependency_test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# Copyright 2003 Dave Abrahams
+# Copyright 2002, 2003, 2005, 2006 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
 from BoostBuild import Tester, List
 
 t = Tester()
