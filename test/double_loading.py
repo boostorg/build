633f85e6a0a07a5ae9f9e0cf618e4b1eef827221
Beman Dawes <bdawes@acm.org>
2007-11-23 17:03:14 +0000

    config, detail, filesystem, system, tools, at 41278.
    [SVN r41316]

--- test/double_loading.py
+++ test/double_loading.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2003. Permission to copy, use, modify, sell and
-#  distribute this software is granted provided this copyright notice appears in
-#  all copies. This software is provided "as is" without express or implied
-#  warranty, and with no claim as to its suitability for any purpose.
+# Copyright 2003 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 from BoostBuild import Tester, List
 import string
