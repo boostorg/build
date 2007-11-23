d4b923c4ca3fbf94956ae1e281e8d6606d7a7a82
Beman Dawes <bdawes@acm.org>
2007-11-23 17:03:14 +0000

    config, detail, filesystem, system, tools, at 41278.
    [SVN r41316]

--- v2/test/gcc_runtime.py
+++ v2/test/gcc_runtime.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2004. Permission to copy, use, modify, sell and
-#  distribute this software is granted provided this copyright notice appears in
-#  all copies. This software is provided "as is" without express or implied
-#  warranty, and with no claim as to its suitability for any purpose.
+# Copyright 2004 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 #  Tests that on gcc, we correctly report problem when static runtime
 #  is requested when building DLL.
--- v2/test/readme.txt
+++ v2/test/readme.txt
@@ -1,3 +1,7 @@
 Comprehensive tests for Boost.Build v2; requires Python. To test, execute:
 
     python test_all.py
+
+# Copyright 2002 Dave Abrahams
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
