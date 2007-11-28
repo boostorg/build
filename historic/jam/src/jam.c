585226c5d2005164d15c5d0180156d8a18838129
Rene Rivera <grafikrobot@gmail.com>
2007-11-28 07:21:49 +0000

    build-system.jam * Reflect added start/end timestamps for actions in xml output. And update action rules for new args.
    execcmd.h
    * Add start/end timestamps to action timing info.
    
    execnt.c
    * Fix filetime_seconds calculation when time is larger than low 32 bit value.
    * Add calc of C time_t from Windows FILETIME.
    * Add start/end timestamps recording to action timing info.
    
    execunix.c
    * Add start/end timestamps recording to action timing info.
    
    jam.c
    * Change JAMDATE to use common ISO date format.
    
    make1.c
    * Redo __TIMING_RULE__ and __ACTION__RULE__ invocations to new argument ordering and added end/result timestamp values.
    
    [SVN r41431]

--- historic/jam/src/jam.c
+++ historic/jam/src/jam.c
@@ -122,6 +122,7 @@
 # include "strings.h"
 # include "expand.h"
 # include "filesys.h"
+# include "output.h"
 
 /* Macintosh is "special" */
 
@@ -372,19 +373,7 @@ int  main( int argc, char **argv, char **arg_environ )
 
     /* Set JAMDATE first */
 
-    {
-        char *date;
-        time_t clock;
-        time( &clock );
-        date = newstr( ctime( &clock ) );
-
-        /* Trim newline from date */
-
-        if( strlen( date ) == 25 )
-            date[ 24 ] = 0;
-
-        var_set( "JAMDATE", list_new( L0, newstr( date ) ), VAR_SET );
-    }
+    var_set( "JAMDATE", list_new( L0, outf_time(time(0)) ), VAR_SET );
 
  
     var_set( "JAM_VERSION",
