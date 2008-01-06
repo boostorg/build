9b97e0ac91773734aaa9cfbac9ba2c876a0d664b
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2008-01-06 00:25:48 +0000

    No functional changes but only stylistic changes such as: comment typo corrections, wrapping lines to 80 characters, indentations, removing trailing spaces, removing empty lines, made tests use Jamfile.jam and Jamroot.jam Boost Build script names, removed unnecessary module imports, etc.
    [SVN r42506]

--- test/conditionals2.py
+++ test/conditionals2.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python
 
-# Copyright 2003 Vladimir Prus 
-# Distributed under the Boost Software License, Version 1.0. 
-# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
+# Copyright 2003 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 #  Regression test: it was possible that due to evaluation of conditional
 #  requirements, two different values of non-free features were present in
 #  property set.
 
-from BoostBuild import Tester, List
+import BoostBuild
 
-t = Tester()
-
-t.write("project-root.jam", "")
+t = BoostBuild.Tester()
 
 t.write("a.cpp", "")
 
-t.write("Jamfile", """ 
-import feature : feature ;
-import common : file-creation-command ;
+t.write("Jamroot.jam", """
+import feature ;
+import common ;
 
-feature the_feature : false true : propagated ;
+feature.feature the_feature : false true : propagated ;
 
 rule maker ( targets * : sources * : properties * )
 {
-    if <the_feature>false in $(properties)
-    && <the_feature>true in $(properties)
+    if <the_feature>false in $(properties) &&
+        <the_feature>true in $(properties)
     {
         EXIT "Oops, two different values of non-free feature" ;
-    }    
-    CMD on $(targets) = [ file-creation-command ] ;
+    }
+    CMD on $(targets) = [ common.file-creation-command ] ;
 }
 
 actions maker
@@ -37,12 +35,9 @@ actions maker
     $(CMD) $(<) ;
 }
 
-make a : a.cpp :  maker : <variant>debug:<the_feature>true ; 
+make a : a.cpp : maker : <variant>debug:<the_feature>true ;
 """)
 
 t.run_build_system()
 
 t.cleanup()
-
-
-
