1703e4f9c2540f08f344b6946365bacb294013e3
Rene Rivera <grafikrobot@gmail.com>
2008-03-15 18:55:28 +0000

    Merge latest BBv2 from trunk to release.
    [SVN r43635]

--- test/abs_workdir.py
+++ test/abs_workdir.py
@@ -2,30 +2,25 @@
 # Testing whether we may run a test in a absolute directories
 # There are no tests for temporary directories as this is implictly tested in a lot of other cases
 
-from BoostBuild import Tester
-import os, string
+import BoostBuild
+import os
+import string
 
-t = Tester(
-      executable="jam"
-    , workdir = os.getcwd()
-    , pass_toolset=0
-    )
+t = BoostBuild.Tester(arguments="pwd", executable="jam", workdir=os.getcwd(),
+    pass_toolset=0)
 
-jamfile="""
+t.write("Jamroot.jam", """
 actions print_pwd { pwd ; }
 print_pwd pwd ;
 Always pwd ;
-"""
+""")
 
-t.write("Jamfile", jamfile)
-t.write("project-root.jam", " ")
-
-t.run_build_system(status=0, extra_args = "pwd")
+t.run_build_system(status=0)
 
 if 'TMP' in os.environ:
-  tmp_dir =os.environ.get('TMP')
+  tmp_dir = os.environ.get('TMP')
 else:
-  tmp_dir ="/tmp"
+  tmp_dir = "/tmp"
 
 if string.rfind(t.stdout(), tmp_dir) != -1:
     t.fail_test(1)
@@ -33,7 +28,7 @@ if string.rfind(t.stdout(), tmp_dir) != -1:
 if string.rfind(t.stdout(), 'build/v2/test') == -1:
     t.fail_test(1)
 
-t.run_build_system(status=1, extra_args = "pwd", subdir ="/must/fail/with/absolute/path",
-                   stderr=None)
+t.run_build_system(status=1, subdir="/must/fail/with/absolute/path",
+    stderr=None)
 t.cleanup
 
--- test/module_actions.py
+++ test/module_actions.py
@@ -6,39 +6,48 @@
 # Distributed under the Boost Software License, Version 1.0. 
 # (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
 
-from BoostBuild import Tester, List
+import BoostBuild
 import os
 import re
 
 spaces_re = re.compile("\ \ +")
 trailing_spaces_re = re.compile("\ +\n")
 
-t = Tester(pass_toolset=0)
+t = BoostBuild.Tester("-d+1", pass_toolset=0)
 
 t.set_tree('module-actions')
 
-expected = r'''A.act t1: X1-t1   
+#   Note that the following string contains some trailing spaces that should not
+# be removed.
+expected_output = """...found 4 targets...
+...updating 3 targets...
+A.act t1
+A.act t1: X1-t1   
+B.act t1
 B.act t1: X1-t1 X2-B  
+act t1
 act t1: X1-t1 X2-global X3-global 
+A.act t2
 A.act t2: X1-A X2-t2  
+B.act t2
 B.act t2:  X2-t2  
+act t2
 act t2: X1-global X2-t2 X3-global 
+A.act t3
 A.act t3: X1-A  X3-t3 
+B.act t3
 B.act t3:  X2-B X3-t3 
+act t3
 act t3: X1-global X2-global X3-t3 
-'''
+...updated 3 targets...
+"""
 
-# On Unixes, call to 'echo 1     2      3' produces '1 2 3' (note spacing)
+# On Unixes, call to 'echo 1     2      3' produces '1 2 3' (note the spacing)
 # Accomodate for that fact.
 if os.name != 'nt':
-    expected = re.sub(spaces_re, " ", expected)
-    expected = re.sub(trailing_spaces_re, "\n", expected)    
+    expected_output = re.sub(spaces_re, " ", expected_output)
+    expected_output = re.sub(trailing_spaces_re, "\n", expected_output)    
 
-# We expect t5 and t7's output to be dumped to stdout
-t.run_build_system(
-    stdout = expected
-)
-
+t.run_build_system(stdout=expected_output)
 t.expect_nothing_more()
 t.cleanup()
-
