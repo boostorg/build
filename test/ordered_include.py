f7298b43cf43426d7aad35c58bbbcbc8a8f7a2d5
Steven Watanabe <steven@providere-consulting.com>
2008-07-18 03:25:20 +0000

    Make c-scanner search directories specified with <include>a&&b
    * tools/buitin.jam
      (c-scanner.__init__) split includes at &&
    
    * test/test_all.py test/ordered_include.py
      Test case for this fix.
    
    
    [SVN r47537]

---
+++ test/ordered_include.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2008 Steven Watanabe
+#
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt) or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+from BoostBuild import Tester
+
+tester = Tester()
+
+tester.write("Jamroot", """
+    obj test : test.cpp : <include>a&&b ;
+""")
+
+tester.write("test.cpp", """
+    #include <test1.hpp>
+    #include <test2.hpp>
+    
+    int main() {
+    }
+""")
+
+tester.write("a/test1.hpp", """
+""")
+
+tester.write("b/test2.hpp", """
+""")
+
+tester.run_build_system()
+
+tester.expect_addition("bin/$toolset/debug/test.obj")
+
+tester.touch("a/test1.hpp")
+tester.run_build_system()
+tester.expect_touch("bin/$toolset/debug/test.obj")
+
+tester.touch("b/test2.hpp")
+tester.run_build_system()
+tester.expect_touch("bin/$toolset/debug/test.obj")
+
+tester.cleanup()
