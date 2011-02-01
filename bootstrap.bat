7df1f2f4afeab456b07542018ad0da49b8251912
Vladimir Prus <ghost@cs.msu.su>
2011-02-01 08:16:47 +0000

    Kill 'engine/src', moving all content to 'engine'.
    [SVN r68592]

--- bootstrap.bat
+++ bootstrap.bat
@@ -6,20 +6,20 @@ REM Distributed under the Boost Software License, Version 1.0.
 REM (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 ECHO Bootstrapping the build engine
-if exist ".\engine\src\bin.ntx86\bjam.exe" del engine\src\bin.ntx86\bjam.exe
-if exist ".\engine\src\bin.ntx86_64\bjam.exe" del engine\src\bin.ntx86_64\bjam.exe
-cd engine\src
+if exist ".\engine\bin.ntx86\bjam.exe" del engine\bin.ntx86\bjam.exe
+if exist ".\engine\bin.ntx86_64\bjam.exe" del engine\bin.ntx86_64\bjam.exe
+cd engine
 
 call .\build.bat %* > ..\..\bootstrap.log
 @ECHO OFF
 cd ..\..
 
-if exist ".\engine\src\bin.ntx86\bjam.exe" (
-   copy .\engine\src\bin.ntx86\bjam.exe . > nul
+if exist ".\engine\bin.ntx86\bjam.exe" (
+   copy .\engine\bin.ntx86\bjam.exe . > nul
    goto :bjam_built)
 
-if exist ".\engine\src\bin.ntx86_64\bjam.exe" (
-   copy .\engine\src\bin.ntx86_64\bjam.exe . > nul
+if exist ".\engine\bin.ntx86_64\bjam.exe" (
+   copy .\engine\bin.ntx86_64\bjam.exe . > nul
    goto :bjam_built)
 
 goto :bjam_failure
