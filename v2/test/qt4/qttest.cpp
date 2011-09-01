b4ecc9190b9c74c5c9dd358ab0d7c77e6d85717b
Jürgen Hunold <jhunold@gmx.eu>
2011-09-01 20:13:34 +0000

    Merge 74058,74157 from ^/trunk ------------------------------------------------------------------------ r74058 | jhunold | 2011-08-25 20:09:59 +0200 (Do, 25 Aug 2011) | 1 line
    Fix #5802: Add response file support for moc.
    ------------------------------------------------------------------------
    r74157 | jhunold | 2011-08-30 15:15:39 +0200 (Di, 30 Aug 2011) | 1 line
    
    Fix: Do not quote INCLUDES in moc response file. refs #5802.
    ------------------------------------------------------------------------
    
    
    [SVN r74186]

--- v2/test/qt4/qttest.cpp
+++ v2/test/qt4/qttest.cpp
@@ -1,4 +1,4 @@
-// (c) Copyright Juergen Hunold 2008
+// (c) Copyright Juergen Hunold 2008-2011
 // Use, modification and distribution is subject to the Boost Software
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -7,11 +7,15 @@
 
 class QtTest: public QObject
 {
+    /*!
+      Test if the moc gets the #define
+    */
+#if defined(TEST_MOCK)
     Q_OBJECT
+#endif
 
 private Q_SLOTS:
     void toUpper();
-
 };
 
 void
