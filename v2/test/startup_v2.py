7f0db73ee3a69515e13b534f978423089324ce1b
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-14 12:27:24 +0000

    Updated Boost Build's startup_v2.py test to cleanly report and there is an unexpected boost-build.jam file found in any of the folders along the test's current working folder path. Before, such a file would break the test making sure Boost Build correctly reports if such a file is missing, and in such a way that made debugging the problem difficult.
    [SVN r78945]

--- v2/test/startup_v2.py
+++ v2/test/startup_v2.py
@@ -11,12 +11,49 @@ import os.path
 import re
 
 
+def check_for_existing_boost_build_jam(t):
+    """
+      This test depends on no boost-build.jam file existing in any of the
+    folders along the current folder's path. If it does exist, not only would
+    this test fail but it could point to a completely wrong Boost Build
+    installation, thus causing headaches when attempting to diagnose the
+    problem. That is why we explicitly check for this scenario.
+
+    """
+    problem = find_up_to_root(t.workdir, "boost-build.jam")
+    if problem:
+        BoostBuild.annotation("misconfiguration", """\
+This test expects to be run from a folder with no 'boost-build.jam' file in any
+of the folders along its path.
+
+Working folder:
+  '%s'
+
+Problematic boost-build.jam found at:
+  '%s'
+
+Please remove this file or change the test's working folder and rerun the test.
+""" % (t.workdir, problem))
+        t.fail_test(1, dump_stdio=False, dump_stack=False)
+
+
+def find_up_to_root(folder, name):
+    last = ''
+    while last != folder:
+        candidate = os.path.join(folder, name)
+        if os.path.exists(candidate):
+            return candidate
+        last = folder
+        folder = os.path.dirname(folder)
+
+
 def match_re(actual, expected):
     return re.match(expected, actual, re.DOTALL) != None
 
 
 t = BoostBuild.Tester(match=match_re, boost_build_path='', pass_toolset=0)
 t.set_tree('startup')
+check_for_existing_boost_build_jam(t)
 
 t.run_build_system(status=1, stdout=
 r'''Unable to load Boost\.Build: could not find "boost-build\.jam"
