fd2c21b14785f5d478d87958bd308af6841e995a
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-15 19:20:21 +0000

    Minor stylistic changes.
    [SVN r78960]

--- test/generators_test.py
+++ test/generators_test.py
@@ -8,18 +8,15 @@
 import BoostBuild
 
 t = BoostBuild.Tester()
-
 t.set_tree("generators-test")
 
 t.run_build_system()
-
-t.expect_addition( "bin/$toolset/debug/" * BoostBuild.List( "a.obj b.obj c.h "
-    + "c.cpp c.obj d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp "
-    + "d_parser.lr0 d_parser.h d_parser_symbols.h x.c x.obj y.x1 y.x2 y.cpp "
-    + "y.obj e.marked_cpp e.positions e.target_cpp e.obj"))
-t.expect_addition("bin/$toolset/debug/a.exe")
-t.expect_addition(["lib/bin/$toolset/debug/c.obj",
-                   "lib/bin/$toolset/debug/auxilliary.lib"])
+t.expect_addition("bin/$toolset/debug/" * BoostBuild.List("a.exe a.obj "
+    "b.obj c.h c.cpp c.obj d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp "
+    "d_parser.lr0 d_parser.h d_parser_symbols.h x.c x.obj y.x1 y.x2 y.cpp "
+    "y.obj e.marked_cpp e.positions e.target_cpp e.obj"))
+t.expect_addition("lib/bin/$toolset/debug/" * BoostBuild.List("c.obj "
+    "auxilliary.lib"))
 
 t.run_build_system(subdir='lib')
 t.expect_addition(["lib/bin/$toolset/debug/auxilliary2.dll"])
