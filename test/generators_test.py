f98d3aef0965fa89837457531e0cc513e258ef16
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-15 22:46:27 +0000

    Made the generators_test.py Boost Build test more detailed.
    [SVN r78962]

--- test/generators_test.py
+++ test/generators_test.py
@@ -13,9 +13,11 @@ t.set_tree("generators-test")
 t.run_build_system()
 t.expect_addition("bin/$toolset/debug/" * BoostBuild.List("a.exe a.obj "
     "b.obj c.h c.cpp c.obj d_parser.whl d_lexer.dlp d_parser.cpp d_lexer.cpp "
-    "d_parser.lr0 d_parser.h d_parser_symbols.h x.c x.obj y.x1 y.x2 y.cpp "
-    "y.obj e.marked_cpp e.positions e.target_cpp e.obj"))
+    "d_lexer.obj d_parser.lr0 d_parser.h d_parser.obj d_parser_symbols.h x.c "
+    "x.obj y.x1 y.x2 y.cpp y.obj e.marked_cpp e.positions e.target_cpp e.obj "
+    "e.exe f.exe obj_1.obj obj_2.obj"))
 t.expect_addition("lib/bin/$toolset/debug/" * BoostBuild.List("c.obj "
     "auxilliary.lib"))
+t.expect_nothing_more()
 
 t.cleanup()
