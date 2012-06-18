83b5bd633273cd1068e7e7115be2fc350500d661
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-18 11:52:09 +0000

    Cleaned up the Boost core_arguments.py test code to reduce duplication.
    [SVN r78980]

--- test/core_arguments.py
+++ test/core_arguments.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 
-# Copyright 2001 Dave Abrahams 
+# Copyright 2001 Dave Abrahams
 # Copyright 2011 Steven Watanabe
-# Distributed under the Boost Software License, Version 1.0. 
-# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 import BoostBuild
+
+
+def test(t, type, input, output):
+    code = ["include echo_args.jam ; echo_%s" % type]
+    if input: code.append(input)
+    code.append(";")
+    t.write("file.jam", " ".join(code))
+    t.run_build_system("-ffile.jam", status=1)
+    t.expect_output_line(output);
+
+
+def test_args(t, *args, **kwargs):
+    test(t, "args", *args, **kwargs)
+
+
+def test_varargs(t, *args, **kwargs):
+    test(t, "varargs", *args, **kwargs)
+
 
 t = BoostBuild.Tester(pass_toolset=0, pass_d0=False)
 
@@ -27,82 +45,34 @@ rule echo_varargs ( a b ? c ? : d + : e * : * )
 }
 """)
 
-t.write("file.jam", "include echo_args.jam ; echo_args ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("* missing argument a");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 2 : 3 : 4 : 5 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("* extra argument 5");
-
-t.write("file.jam", "include echo_args.jam ; echo_args a b c1 c2 : d ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("* extra argument c2");
+test_args(t, "", "* missing argument a")
+test_args(t, "1 2 : 3 : 4 : 5", "* extra argument 5")
+test_args(t, "a b c1 c2 : d", "* extra argument c2")
 
 # Check modifier '?'
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 2 3 : 4 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= 2 c= 3 : d= 4 : e=");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 2 : 3 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= 2 c= : d= 3 : e=");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e=");
+test_args(t, "1 2 3 : 4", "a= 1 b= 2 c= 3 : d= 4 : e=")
+test_args(t, "1 2 : 3", "a= 1 b= 2 c= : d= 3 : e=")
+test_args(t, "1 2 : 3", "a= 1 b= 2 c= : d= 3 : e=")
+test_args(t, "1 : 2", "a= 1 b= c= : d= 2 : e=")
 
 # Check modifier '+'
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("* missing argument d");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 3 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 3 : e=");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 3 4 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 3 4 : e=");
+test_args(t, "1", "* missing argument d")
+test_args(t, "1 : 2 3", "a= 1 b= c= : d= 2 3 : e=")
+test_args(t, "1 : 2 3 4", "a= 1 b= c= : d= 2 3 4 : e=")
 
 # Check modifier '*'
+test_args(t, "1 : 2 : 3", "a= 1 b= c= : d= 2 : e= 3")
+test_args(t, "1 : 2 : 3 4", "a= 1 b= c= : d= 2 : e= 3 4")
+test_args(t, "1 : 2 : 3 4 5", "a= 1 b= c= : d= 2 : e= 3 4 5")
 
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 : 3 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 : 3 4 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4");
-
-t.write("file.jam", "include echo_args.jam ; echo_args 1 : 2 : 3 4 5 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5");
-
-#
 # Check varargs
-#
-
-t.write("file.jam", "include echo_args.jam ; echo_varargs 1 : 2 : 3 4 5 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5");
-
-t.write("file.jam", "include echo_args.jam ; echo_varargs 1 : 2 : 3 4 5 : 6 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6");
-
-t.write("file.jam", "include echo_args.jam ; echo_varargs 1 : 2 : 3 4 5 : 6 7 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7");
-
-t.write("file.jam", "include echo_args.jam ; echo_varargs 1 : 2 : 3 4 5 : 6 7 : 8 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7 : 8");
-
-t.write("file.jam", "include echo_args.jam ; echo_varargs 1 : 2 : 3 4 5 : 6 7 : 8 : 9 ;")
-t.run_build_system("-ffile.jam", status=1)
-t.expect_output_line("a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7 : 8 : 9");
+test_varargs(t, "1 : 2 : 3 4 5", "a= 1 b= c= : d= 2 : e= 3 4 5")
+test_varargs(t, "1 : 2 : 3 4 5 : 6", "a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6")
+test_varargs(t, "1 : 2 : 3 4 5 : 6 7",
+    "a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7")
+test_varargs(t, "1 : 2 : 3 4 5 : 6 7 : 8",
+    "a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7 : 8")
+test_varargs(t, "1 : 2 : 3 4 5 : 6 7 : 8 : 9",
+    "a= 1 b= c= : d= 2 : e= 3 4 5 : rest= 6 7 : 8 : 9")
 
 t.cleanup()
