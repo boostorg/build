f7fb4c909c0d8217d12db2045619e6ce56e4a458
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-18 23:31:40 +0000

    Minor stylistic end-user error message update in Boost Build's execnt.c module.
    [SVN r78993]

--- v2/engine/execnt.c
+++ v2/engine/execnt.c
@@ -475,7 +475,7 @@ void exec_cmd
                 )
             )
         {
-            perror( "CreateProcess" );
+            perror( "CreateProcessA" );
             exit( EXITBAD );
         }
     }
