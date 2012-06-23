c5acd8299e1e941058b12a8ea0ee70d98e33408a
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-23 23:41:07 +0000

    Minor stylistic changes in Boost Jam's strings.c module.
    [SVN r79038]

--- src/engine/strings.c
+++ src/engine/strings.c
@@ -4,10 +4,10 @@
 
 #include "jam.h"
 #include "strings.h"
+
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <stdio.h>
 
 
 #ifndef NDEBUG
@@ -21,18 +21,18 @@ static void assert_invariants( string * self )
     {
         assert( self->size == 0 );
         assert( self->capacity == 0 );
-        assert( self->opt[0] == 0 );
+        assert( self->opt[ 0 ] == 0 );
         return;
     }
 
     assert( self->size < self->capacity );
-    assert( ( self->capacity <= sizeof(self->opt) ) == ( self->value == self->opt ) );
+    assert( ( self->capacity <= sizeof( self->opt ) ) == ( self->value == self->opt ) );
     assert( strlen( self->value ) == self->size );
 
     for ( i = 0; i < 4; ++i )
     {
-        assert( self->magic[i] == JAM_STRING_MAGIC );
-        assert( self->value[self->capacity + i] == JAM_STRING_MAGIC );
+        assert( self->magic[ i ] == JAM_STRING_MAGIC );
+        assert( self->value[ self->capacity + i ] == JAM_STRING_MAGIC );
     }
 }
 #else
@@ -40,17 +40,19 @@ static void assert_invariants( string * self )
 # define assert_invariants(x) do {} while (0)
 #endif
 
+
 void string_new( string * s )
 {
     s->value = s->opt;
     s->size = 0;
-    s->capacity = sizeof(s->opt);
-    s->opt[0] = 0;
+    s->capacity = sizeof( s->opt );
+    s->opt[ 0 ] = 0;
 #ifndef NDEBUG
-    memset(s->magic, JAM_STRING_MAGIC, sizeof(s->magic));
+    memset( s->magic, JAM_STRING_MAGIC, sizeof( s->magic ) );
 #endif
     assert_invariants( s );
 }
+
 
 void string_free( string * s )
 {
@@ -60,12 +62,13 @@ void string_free( string * s )
     string_new( s );
 }
 
+
 static void string_reserve_internal( string * self, size_t capacity )
 {
     if ( self->value == self->opt )
     {
         self->value = (char*)BJAM_MALLOC_ATOMIC( capacity + JAM_STRING_MAGIC_SIZE );
-        self->value[0] = 0;
+        self->value[ 0 ] = 0;
         strncat( self->value, self->opt, sizeof(self->opt) );
         assert( strlen( self->value ) <= self->capacity ); /* This is a regression test */
     }
@@ -79,6 +82,7 @@ static void string_reserve_internal( string * self, size_t capacity )
     self->capacity = capacity;
 }
 
+
 void string_reserve( string * self, size_t capacity )
 {
     assert_invariants( self );
@@ -87,6 +91,7 @@ void string_reserve( string * self, size_t capacity )
     string_reserve_internal( self, capacity );
     assert_invariants( self );
 }
+
 
 static void extend_full( string * self, char const * start, char const * finish )
 {
@@ -97,9 +102,10 @@ static void extend_full( string * self, char const * start, char const * finish 
         new_capacity <<= 1;
     string_reserve_internal( self, new_capacity );
     memcpy( self->value + old_size, start, new_size - old_size );
-    self->value[new_size] = 0;
+    self->value[ new_size ] = 0;
     self->size = new_size;
 }
+
 
 void string_append( string * self, char const * rhs )
 {
@@ -116,11 +122,10 @@ void string_append( string * self, char const * rhs )
         self->size = p - self->value;
     }
     else
-    {
         extend_full( self, rhs, rhs + strlen(rhs) );
-    }
     assert_invariants( self );
 }
+
 
 void string_append_range( string * self, char const * start, char const * finish )
 {
@@ -137,11 +142,10 @@ void string_append_range( string * self, char const * start, char const * finish
         self->size = p - self->value;
     }
     else
-    {
         extend_full( self, start, finish );
-    }
     assert_invariants( self );
 }
+
 
 void string_copy( string * s, char const * rhs )
 {
@@ -153,46 +157,49 @@ void string_truncate( string * self, size_t n )
 {
     assert_invariants( self );
     assert( n <= self->capacity );
-    self->value[self->size = n] = 0;
+    self->value[ self->size = n ] = 0;
     assert_invariants( self );
 }
+
 
 void string_pop_back( string * self )
 {
     string_truncate( self, self->size - 1 );
 }
 
+
 void string_push_back( string * self, char x )
 {
     string_append_range( self, &x, &x + 1 );
 }
 
+
 char string_back( string * self )
 {
     assert_invariants( self );
-    return self->value[self->size - 1];
+    return self->value[ self->size - 1 ];
 }
+
 
 #ifndef NDEBUG
 void string_unit_test()
 {
-    string s[1];
+    string s[ 1 ];
     int i;
-    char buffer[sizeof(s->opt) * 2 + 2];
-    int limit = sizeof(buffer) > 254 ? 254 : sizeof(buffer);
+    char buffer[ sizeof( s->opt ) * 2 + 2 ];
+    int limit = sizeof( buffer ) > 254 ? 254 : sizeof( buffer );
 
-    string_new(s);
+    string_new( s );
 
     for ( i = 0; i < limit; ++i )
-        string_push_back( s, (char)(i + 1) );
+        string_push_back( s, (char)( i + 1 ) );
 
-    for (i = 0; i < limit; ++i)
+    for ( i = 0; i < limit; ++i )
     {
         assert( i < s->size );
-        assert( s->value[i] == (char)(i + 1));
+        assert( s->value[ i ] == (char)( i + 1 ) );
     }
 
-    string_free(s);
+    string_free( s );
 }
 #endif
-
