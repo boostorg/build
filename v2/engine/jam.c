6c27b48f7335a5f6eddc4b14cc2e909d12a66a94
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-24 11:33:05 +0000

    Boost Jam code cleanup - minor stylistic changes in jam.c & jam.h modules.
    [SVN r79053]

--- v2/engine/jam.c
+++ v2/engine/jam.c
@@ -5,9 +5,9 @@
  *
  * This file is part of jam.
  *
- * License is hereby granted to use this software and distribute it
- * freely, as long as this copyright notice is retained and modifications
- * are clearly marked.
+ * License is hereby granted to use this software and distribute it freely, as
+ * long as this copyright notice is retained and modifications are clearly
+ * marked.
  *
  * ALL WARRANTIES ARE HEREBY DISCLAIMED.
  */
@@ -50,8 +50,8 @@
  *                             filesys
  *
  *
- * The support routines are called by all of the above, but themselves
- * are layered thus:
+ * The support routines are called by all of the above, but themselves are
+ * layered thus:
  *
  *                     variable|expand
  *                      /      |   |
@@ -72,8 +72,8 @@
  *  builtins.c - jam's built-in rules
  *  command.c - maintain lists of commands
  *  compile.c - compile parsed jam statements
- *  execunix.c - execute a shell script on UNIX
- *  file*.c - scan directories and archives on *
+ *  exec*.c - execute a shell script on a specific OS
+ *  file*.c - scan directories and archives on a specific OS
  *  hash.c - simple in-memory hashing routines
  *  hdrmacro.c - handle header file parsing for filename macro definitions
  *  headers.c - handle #includes in source files
@@ -85,7 +85,7 @@
  *  object.c - string manipulation routines
  *  option.c - command line option processing
  *  parse.c - make and destroy parse trees as driven by the parser
- *  path*.c - manipulate file names on *
+ *  path*.c - manipulate file names on a specific OS
  *  hash.c - simple in-memory hashing routines
  *  regexp.c - Henry Spencer's regexp
  *  rules.c - access to RULEs, TARGETs, and ACTIONs
@@ -93,12 +93,6 @@
  *  search.c - find a target along $(SEARCH) or $(LOCATE)
  *  timestamp.c - get the timestamp of a file or archive member
  *  variable.c - handle jam multi-element variables
- *
- * 05/04/94 (seiwald) - async multiprocess (-j) support
- * 02/08/95 (seiwald) - -n implies -d2.
- * 02/22/95 (seiwald) - -v for version info.
- * 09/11/00 (seiwald) - PATCHLEVEL folded into VERSION.
- * 01/10/01 (seiwald) - pathsys.h split from filesys.h
  */
 
 
@@ -216,26 +210,26 @@ int anyhow = 0;
 
 void regex_done();
 
-const char *saved_argv0;
+char const * saved_argv0;
 
 int main( int argc, char * * argv, char * * arg_environ )
 {
     int                     n;
     char                  * s;
-    struct bjam_option      optv[N_OPTS];
+    struct bjam_option      optv[ N_OPTS ];
     char            const * all = "all";
     int                     status;
     int                     arg_c = argc;
     char          *       * arg_v = argv;
-    char            const * progname = argv[0];
+    char            const * progname = argv[ 0 ];
     module_t              * environ_module;
 
-    saved_argv0 = argv[0];
+    saved_argv0 = argv[ 0 ];
 
     BJAM_MEM_INIT();
 
 # ifdef OS_MAC
-    InitGraf(&qd.thePort);
+    InitGraf( &qd.thePort );
 # endif
 
     --argc;
@@ -269,7 +263,7 @@ int main( int argc, char * * argv, char * * arg_environ )
     {
         printf( "Boost.Jam  " );
         printf( "Version %s. %s.\n", VERSION, OSMINOR );
-        printf( "   Copyright 1993-2002 Christopher Seiwald and Perforce Software, Inc.  \n" );
+        printf( "   Copyright 1993-2002 Christopher Seiwald and Perforce Software, Inc.\n" );
         printf( "   Copyright 2001 David Turner.\n" );
         printf( "   Copyright 2001-2004 David Abrahams.\n" );
         printf( "   Copyright 2002-2008 Rene Rivera.\n" );
@@ -280,7 +274,10 @@ int main( int argc, char * * argv, char * * arg_environ )
 
     /* Pick up interesting options. */
     if ( ( s = getoptval( optv, 'n', 0 ) ) )
-        globs.noexec++, globs.debug[2] = 1;
+    {
+        ++globs.noexec;
+        globs.debug[ 2 ] = 1;
+    }
 
     if ( ( s = getoptval( optv, 'p', 0 ) ) )
     {
@@ -288,11 +285,10 @@ int main( int argc, char * * argv, char * * arg_environ )
          * stdout and stderr.
          */
         globs.pipe_action = atoi( s );
-        if ( ( 3 < globs.pipe_action ) || ( globs.pipe_action < 0 ) )
+        if ( globs.pipe_action < 0 || 3 < globs.pipe_action )
         {
-            printf(
-                "Invalid pipe descriptor '%d', valid values are -p[0..3].\n",
-                globs.pipe_action );
+            printf( "Invalid pipe descriptor '%d', valid values are -p[0..3]."
+                "\n", globs.pipe_action );
             exit( EXITBAD );
         }
     }
@@ -306,10 +302,10 @@ int main( int argc, char * * argv, char * * arg_environ )
     if ( ( s = getoptval( optv, 'j', 0 ) ) )
     {
         globs.jobs = atoi( s );
-        if (globs.jobs == 0)
+        if ( !globs.jobs )
         {
-            printf("Invalid value for the '-j' option.\n");
-            exit(EXITBAD);
+            printf( "Invalid value for the '-j' option.\n" );
+            exit( EXITBAD );
         }
     }
 
@@ -343,9 +339,9 @@ int main( int argc, char * * argv, char * * arg_environ )
         /* n turns on levels 1-n. */
         /* +n turns on level n. */
         if ( *s == '+' )
-            globs.debug[i] = 1;
+            globs.debug[ i ] = 1;
         else while ( i )
-            globs.debug[i--] = 1;
+            globs.debug[ i-- ] = 1;
     }
 
     constants_init();
@@ -442,7 +438,7 @@ int main( int argc, char * * argv, char * * arg_environ )
         /* Load up variables set on command line. */
         for ( n = 0; ( s = getoptval( optv, 's', n ) ); ++n )
         {
-            char *symv[2];
+            char *symv[ 2 ];
             symv[ 0 ] = s;
             symv[ 1 ] = 0;
             var_defines( root_module(), symv, 1 );
@@ -452,9 +448,8 @@ int main( int argc, char * * argv, char * * arg_environ )
         /* Set the ARGV to reflect the complete list of arguments of invocation.
          */
         for ( n = 0; n < arg_c; ++n )
-        {
-            var_set( root_module(), constant_ARGV, list_new( object_new( arg_v[n] ) ), VAR_APPEND );
-        }
+            var_set( root_module(), constant_ARGV, list_new( object_new(
+                arg_v[n] ) ), VAR_APPEND );
 
         /* Initialize built-in rules. */
         load_builtins();
@@ -470,16 +465,14 @@ int main( int argc, char * * argv, char * * arg_environ )
             }
             else
             {
-                OBJECT * target = object_new( arg_v[ n ] );
+                OBJECT * const target = object_new( arg_v[ n ] );
                 mark_target_for_updating( target );
                 object_free( target );
             }
         }
 
         if ( list_empty( targets_to_update() ) )
-        {
             mark_target_for_updating( constant_all );
-        }
 
         /* Parse ruleset. */
         {
@@ -487,15 +480,13 @@ int main( int argc, char * * argv, char * * arg_environ )
             frame_init( frame );
             for ( n = 0; ( s = getoptval( optv, 'f', n ) ); ++n )
             {
-                OBJECT * filename = object_new( s );
+                OBJECT * const filename = object_new( s );
                 parse_file( filename, frame );
                 object_free( filename );
             }
 
             if ( !n )
-            {
                 parse_file( constant_plus, frame );
-            }
         }
 
         status = yyanyerrors();
@@ -503,7 +494,7 @@ int main( int argc, char * * argv, char * * arg_environ )
         /* Manually touch -t targets. */
         for ( n = 0; ( s = getoptval( optv, 't', n ) ); ++n )
         {
-            OBJECT * target = object_new( s );
+            OBJECT * const target = object_new( s );
             touch_target( target );
             object_free( target );
         }
@@ -520,52 +511,36 @@ int main( int argc, char * * argv, char * * arg_environ )
         }
 
         /* The build system may set the PARALLELISM variable to override -j
-           options.  */
+         * options.
+         */
         {
-            LIST *p = L0;
-            p = var_get ( root_module(), constant_PARALLELISM );
+            LIST * const p = var_get( root_module(), constant_PARALLELISM );
             if ( !list_empty( p ) )
             {
-                int j = atoi( object_str( list_front( p ) ) );
+                int const j = atoi( object_str( list_front( p ) ) );
                 if ( j == -1 )
-                {
-                    printf( "Invalid value of PARALLELISM: %s\n", object_str( list_front( p ) ) );
-                }
+                    printf( "Invalid value of PARALLELISM: %s\n", object_str(
+                        list_front( p ) ) );
                 else
-                {
                     globs.jobs = j;
-                }
             }
         }
 
         /* KEEP_GOING overrides -q option. */
         {
-            LIST *p = L0;
-            p = var_get( root_module(), constant_KEEP_GOING );
+            LIST * const p = var_get( root_module(), constant_KEEP_GOING );
             if ( !list_empty( p ) )
-            {
-                int v = atoi( object_str( list_front( p ) ) );
-                if ( v == 0 )
-                    globs.quitquick = 1;
-                else
-                    globs.quitquick = 0;
-            }
+                globs.quitquick = atoi( object_str( list_front( p ) ) ) ? 0 : 1;
         }
 
         /* Now make target. */
         {
             PROFILE_ENTER( MAIN_MAKE );
-
-            LIST * targets = targets_to_update();
+            LIST * const targets = targets_to_update();
             if ( !list_empty( targets ) )
-            {
                 status |= make( targets, anyhow );
-            }
             else
-            {
                 status = last_update_now_status;
-            }
-
             PROFILE_EXIT( MAIN_MAKE );
         }
 
@@ -575,7 +550,7 @@ int main( int argc, char * * argv, char * * arg_environ )
     if ( DEBUG_PROFILE )
         profile_dump();
 
-    
+
 #ifdef OPT_HEADER_CACHE_EXT
     hcache_done();
 #endif
@@ -610,56 +585,56 @@ int main( int argc, char * * argv, char * * arg_environ )
     return status ? EXITBAD : EXITOK;
 }
 
+
+/*
+ * executable_path()
+ */
+
 #if defined(_WIN32)
 #include <windows.h>
-char *executable_path(const char *argv0) {
-    char buf[1024];
-    DWORD ret = GetModuleFileName(NULL, buf, sizeof(buf));
-    if (ret == 0 || ret == sizeof(buf)) return NULL;
-    return strdup (buf);
+char * executable_path( char const * argv0 )
+{
+    char buf[ 1024 ];
+    DWORD const ret = GetModuleFileName( NULL, buf, sizeof( buf ) );
+    return ( !ret || ret == sizeof( buf ) ) ? NULL : strdup( buf );
 }
 #elif defined(__APPLE__)  /* Not tested */
 #include <mach-o/dyld.h>
-char *executable_path(const char *argv0) {
-    char buf[1024];
-    uint32_t size = sizeof(buf);
-    int ret = _NSGetExecutablePath(buf, &size);
-    if (ret != 0) return NULL;
-    return strdup(buf);
+char *executable_path( char const * argv0 )
+{
+    char buf[ 1024 ];
+    uint32_t size = sizeof( buf );
+    return _NSGetExecutablePath( buf, &size ) ? NULL : strdup( buf );
 }
 #elif defined(sun) || defined(__sun) /* Not tested */
 #include <stdlib.h>
 
-char *executable_path(const char *argv0) {
-    return strdup(getexecname());
+char * executable_path( char const * argv0 )
+{
+    return strdup( getexecname() );
 }
 #elif defined(__FreeBSD__)
 #include <sys/sysctl.h>
-char *executable_path(const char *argv0) {
-    int mib[4];
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_PROC;
-    mib[2] = KERN_PROC_PATHNAME;
-    mib[3] = -1;
-    char buf[1024];
-    size_t size = sizeof(buf);
-    sysctl(mib, 4, buf, &size, NULL, 0);
-    if (size == 0 || size == sizeof(buf)) return NULL;
-    return strndup(buf, size);
+char * executable_path( char const * argv0 )
+{
+    int mib[ 4 ] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
+    char buf[ 1024 ];
+    size_t size = sizeof( buf );
+    sysctl( mib, 4, buf, &size, NULL, 0 );
+    return ( !size || size == sizeof( buf ) ) ? NULL : strndup( buf, size );
 }
 #elif defined(__linux__)
 #include <unistd.h>
-char *executable_path(const char *argv0) {
-    char buf[1024];
+char * executable_path( char const * argv0 )
+{
+    char buf[ 1024 ];
     ssize_t ret = readlink("/proc/self/exe", buf, sizeof(buf));
-    if (ret == 0 || ret == sizeof(buf)) return NULL;
-    return strndup(buf, ret);
+    return ( !ret || ret == sizeof( buf ) ) ? NULL : strndup( buf, ret );
 }
 #else
-char *executable_path(const char *argv0) {
-    /* If argv0 is absolute path, assume it's the right absolute path. */
-    if (argv0[0] == '/')
-        return strdup(argv0);
-    return NULL;
+char * executable_path( char const * argv0 )
+{
+    /* If argv0 is an absolute path, assume it is the right absolute path. */
+    return argv0[ 0 ] == '/' ? strdup( argv0 ) : NULL;
 }
 #endif
