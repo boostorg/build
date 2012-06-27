8064d1bd6e37209255f3a338762c33fecd90dd86
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-06-27 22:20:33 +0000

    Added a new Boost Build test - testing the rc.jam toolset. Guards against a regression causing targets depending on a .rc script file to rebuild every time if the .rc script includes a resource file newer than itself.
    [SVN r79148]

---
+++ v2/test/test_rc.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Copyright 2012 Jurko Gospodnetic
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
+# Tests rc toolset behaviour.
+
+import BoostBuild
+
+
+def included_resource_newer_than_rc_script():
+    """
+      When a .rc script file includes another resource file - the resource file
+    being newer than the .rc script file should not cause the .rc script file
+    to be considered old and force all of its dependents to rebuild.
+
+    """
+    t = BoostBuild.Tester("-d1", pass_d0=False)
+
+    t.write("jamroot.jam", """\
+ECHO {{{ [ modules.peek : XXX ] [ modules.peek : NOEXEC ] }}} ;
+obj xxx : xxx.rc ;
+""")
+    t.write("xxx.rc", '1 MESSAGETABLE "xxx.bin"\n')
+    t.write("xxx.bin", "foo")
+
+    output_line = "*%s*" % t.adjust_suffix("xxx_res.obj")
+
+    def test1(n, expect, noexec=False):
+        params = "-sXXX=%d" % n
+        if noexec:
+            params += " -n -sNOEXEC=NOEXEC"
+        t.run_build_system(params)
+        t.expect_output_line("*NOEXEC*", noexec)
+        t.expect_output_line(output_line, expect)
+        if expect and not noexec:
+            expect("bin/$toolset/debug/xxx_res.obj")
+        t.expect_nothing_more()
+
+    def test(n, expect):
+        test1(n, expect, noexec=True)
+        test1(n, expect)
+
+    test(1, t.expect_addition)
+    test(2, None)
+    t.touch("xxx.bin")
+    test(3, t.expect_touch)
+    test(4, None)
+
+    t.cleanup()
+
+
+included_resource_newer_than_rc_script()
