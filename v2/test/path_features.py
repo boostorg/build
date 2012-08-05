58c4aa2d0386e5a0c551a840ee47ba8c7cff6a6f
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-05 19:45:28 +0000

    Internal Boost Build path_features.py test cleanup - minor stylistic changes.
    [SVN r79883]

--- v2/test/path_features.py
+++ v2/test/path_features.py
@@ -3,7 +3,8 @@
 # Copyright 2003 Dave Abrahams
 # Copyright 2002, 2003, 2004 Vladimir Prus
 # Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
 
 import BoostBuild
 
@@ -12,19 +13,19 @@ def test_basic():
 
     t.write("jamroot.jam", "lib a : a.cpp : <include>. ;")
     t.write("a.cpp", """\
-    #include <a.h>
-    void
-    # ifdef _WIN32
-    __declspec(dllexport)
-    # endif
-    foo() {}
-    """)
+#include <a.h>
+void
+# ifdef _WIN32
+__declspec(dllexport)
+# endif
+foo() {}
+""")
     t.write("a.h", "//empty file\n")
     t.write("d/jamfile.jam", "exe b : b.cpp ..//a ;")
     t.write("d/b.cpp", """\
-    void foo();
-    int main() { foo(); }
-    """)
+void foo();
+int main() { foo(); }
+""")
     t.run_build_system(subdir="d")
 
     # Path features with condition.
@@ -35,13 +36,13 @@ def test_basic():
 
     # Path features with condition in usage requirements.
     t.write("jamroot.jam", """\
-    lib a : a.cpp : <include>. : : <variant>debug:<include>. ;
-    """)
+lib a : a.cpp : <include>. : : <variant>debug:<include>. ;
+""")
     t.write("d/b.cpp", """\
-    #include <a.h>
-    void foo();
-    int main() { foo(); }
-    """)
+#include <a.h>
+void foo();
+int main() { foo(); }
+""")
     t.rm("d/bin")
     t.run_build_system(subdir="d")
 
@@ -50,23 +51,23 @@ def test_basic():
 
 def test_absolute_paths():
     """
-      Test that absolute paths inside requirements are ok. The problems appeared
-    only when building targets in subprojects.
+      Test that absolute paths inside requirements are ok. The problems
+    appeared only when building targets in subprojects.
 
     """
     t = BoostBuild.Tester()
 
     t.write("jamroot.jam", "build-project x ;")
     t.write("x/jamfile.jam", """\
-    local pwd = [ PWD ] ;
-    project : requirements <include>$(pwd)/x/include ;
-    exe m : m.cpp : <include>$(pwd)/x/include2 ;
-    """)
+local pwd = [ PWD ] ;
+project : requirements <include>$(pwd)/x/include ;
+exe m : m.cpp : <include>$(pwd)/x/include2 ;
+""")
     t.write("x/m.cpp", """\
-    #include <h1.hpp>
-    #include <h2.hpp>
-    int main() {}
-    """)
+#include <h1.hpp>
+#include <h2.hpp>
+int main() {}
+""")
     t.write("x/include/h1.hpp", "\n")
     t.write("x/include2/h2.hpp", "\n")
 
@@ -84,9 +85,9 @@ def test_ordered_paths():
     t.write("jamroot.jam", "build-project sub ;")
     t.write("sub/jamfile.jam", "exe a : a.cpp : <include>../h1&&../h2 ;")
     t.write("sub/a.cpp", """\
-    #include <header.h>
-    int main() { return OK; }
-    """)
+#include <header.h>
+int main() { return OK; }
+""")
     t.write("h2/header.h", "int const OK = 0;\n")
     t.run_build_system()
     t.expect_addition("sub/bin/$toolset/debug/a.exe")
