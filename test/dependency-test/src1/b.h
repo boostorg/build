7563ac78779e68727f479d2251cd457c91fc7fcc
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-13 17:55:20 +0000

    Internal Boost Build dependency_test.py test cleanup - removed some old and no longer relevant comments (from the time before the <dependency> feature got renamed to <implicit-dependency>), better commented the <implicit-dependency> feature usage, removed redundant ignoring of *.tds Borland debugger symbol files (already done internally in the BoostBuild.py testing system implementation module), minor stylistic changes.
    [SVN r80012]

--- test/dependency-test/src1/b.h
+++ test/dependency-test/src1/b.h
@@ -1,10 +1,6 @@
-//  Copyright (c) 2003 Vladimir Prus
-//
-//  Distributed under the Boost Software License, Version 1.0. (See
-//  accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-//
-//  http://www.boost.org
-// 
+// Copyright (c) 2003 Vladimir Prus
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include "c.h"
