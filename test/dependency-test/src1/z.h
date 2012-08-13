0c327209125ef5ef167f0acbe0718bee034bd886
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-13 18:16:14 +0000

    Cleaned up internal Boost Build dependency_test.py test - made the test script create all of its needed test project files at run-time instead of having them maintained as actual Boost Build project files.
    [SVN r80013]

--- test/dependency-test/src1/z.h
+++
@@ -1,6 +0,0 @@
-/* Copyright 2003, 2004, 2006 Vladimir Prus                   */
-/* Distributed under the Boost Software License, Version 1.0. */
-/* (See accompanying file LICENSE_1_0.txt or copy at          */
-/* http://www.boost.org/LICENSE_1_0.txt)                      */
-
-extern int dummy_variable_suppressing_empty_file_warning_on_hp_cxx_compiler;
