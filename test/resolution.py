157384906d1b3a52e92d726b41ce31d7e8b07354
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-14 04:48:11 +0000

    Internal Boost Build resolution.py test cleanup - minor stylistic change.
    [SVN r80020]

--- test/resolution.py
+++ test/resolution.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # Copyright (C) Vladimir Prus 2006.
-# Distributed under the Boost Software License, Version 1.0. (See
-# accompanying file LICENSE_1_0.txt or copy at
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
 # Tests for the target id resolution process.
