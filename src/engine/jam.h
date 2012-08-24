52fe19ba914d8954a405c2364d230b150280208c
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-24 23:07:46 +0000

    Main Boost Jam jam.h header file cleanup - removed the never used DOWNSHIFT_PATHS #define, minor stylistic changes.
    [SVN r80178]

--- src/engine/jam.h
+++ src/engine/jam.h
@@ -4,10 +4,11 @@
  * This file is part of Jam - see jam.c for Copyright information.
  */
 
-/*  This file is ALSO:
- *  Copyright 2001-2004 David Abrahams.
- *  Distributed under the Boost Software License, Version 1.0.
- *  (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+/* This file is ALSO:
+ * Copyright 2001-2004 David Abrahams.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
  */
 
 /*
@@ -30,14 +31,14 @@
 
 #ifdef NT
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <fcntl.h>
 #include <malloc.h>
 #ifndef __MWERKS__
     #include <memory.h>
 #endif
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <string.h>
 #include <time.h>
@@ -50,7 +51,6 @@
 #define USE_EXECNT
 #define USE_PATHUNIX
 #define PATH_DELIM '\\'
-#define DOWNSHIFT_PATHS
 
 /* AS400 cross-compile from NT. */
 
@@ -68,7 +68,8 @@
     #undef HAVE_POPEN
 #endif
 
-# endif
+#endif  /* #ifdef NT */
+
 
 /*
  * Windows MingW32
@@ -94,9 +95,9 @@
 #define USE_EXECUNIX
 #define USE_PATHUNIX
 #define PATH_DELIM '\\'
-#define DOWNSHIFT_PATHS
 
-#endif
+#endif  /* #ifdef MINGW */
+
 
 /*
  * God fearing UNIX.
@@ -324,7 +325,8 @@
     #include <malloc.h>
 #endif
 
-#endif
+#endif  /* #ifndef OSMINOR */
+
 
 /*
  * OSPLAT definitions - suppressed when it is a one-of-a-kind.
@@ -362,7 +364,6 @@
     #define OSPLAT "OSPLAT=X86_64"
 #endif
 
-
 #if defined( __sparc__ ) || \
     defined( __sparc   )
     #define OSPLAT "OSPLAT=SPARC"
@@ -387,6 +388,7 @@
 #ifndef OSPLAT
     #define OSPLAT ""
 #endif
+
 
 /*
  * Jam implementation misc.
@@ -430,8 +432,12 @@ struct globs
     long   timeout;             /* number of seconds to limit actions to,
                                  * default 0 for no limit.
                                  */
-    int    dart;                /* output build and test results formatted for Dart */
-    int    max_buf;             /* maximum amount of output saved from target (kb) */
+    int    dart;                /* output build and test results formatted for
+                                 * Dart
+                                 */
+    int    max_buf;             /* maximum amount of output saved from target
+                                 * (kb)
+                                 */
 };
 
 extern struct globs globs;
