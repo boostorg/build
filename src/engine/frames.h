08eaf3b0661f39fd73423a459feafdd3783d2323
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-25 15:48:07 +0000

    Internal Boost Jam cleanup - minor stylistic copyright comment change.
    [SVN r80206]

--- src/engine/frames.h
+++ src/engine/frames.h
@@ -1,8 +1,10 @@
 /*
- *  Copyright 2001-2004 David Abrahams.
- *  Distributed under the Boost Software License, Version 1.0.
- *  (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+ * Copyright 2001-2004 David Abrahams.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
  */
+
 #ifndef FRAMES_DWA20011021_H
 #define FRAMES_DWA20011021_H
 
