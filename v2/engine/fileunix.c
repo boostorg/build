b4e9a7d24f4d6cc6fc846a4e62dad817fc414d7d
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-27 11:41:15 +0000

    Corrected a 'dropping qualifiers' compiler warning displayed when compiling Boost Jam's fileunix.c module using the intel compiler.
    [SVN r80260]

--- v2/engine/fileunix.c
+++ v2/engine/fileunix.c
@@ -169,7 +169,10 @@ void file_dirscan_( file_info_t * const d, scanback func, void * closure )
 
 int file_mkdir( char const * const path )
 {
-    return mkdir( path, 0777 );
+    /* Explicit cast to remove const modifiers and avoid related compiler
+     * warnings displayed when using the intel compiler.
+     */
+    return mkdir( (char *)path, 0777 );
 }
 
 
