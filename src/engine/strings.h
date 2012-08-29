982ad4011f383ae0f9b0847b27e5a16ea03196f4
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-08-29 13:24:05 +0000

    Internal Boost Jam cleanup - minor stylistic changes.
    [SVN r80301]

--- src/engine/strings.h
+++ src/engine/strings.h
@@ -1,11 +1,14 @@
+/*
+ * Copyright 2004. David Abrahams
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #ifndef STRINGS_DWA20011024_H
-# define STRINGS_DWA20011024_H
+#define STRINGS_DWA20011024_H
 
-/* Copyright David Abrahams 2004. Distributed under the Boost */
-/* Software License, Version 1.0. (See accompanying */
-/* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
-
-# include <stddef.h>
+#include <stddef.h>
 
 typedef struct string
 {
