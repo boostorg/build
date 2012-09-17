ddb28918c8e7f2c048573ac2a2bc7b5403a3b54d
Jurko Gospodnetić <jurko.gospodnetic@pke.hr>
2012-09-17 10:36:59 +0000

    Internal Boost Build cleanup - minor stylistic changes.
    [SVN r80556]

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Copyright 2004 Aleksey Gurtovoy
-# Copyright 2006 Rene Rivera 
-# Copyright 2003, 2004, 2005, 2006 Vladimir Prus 
-# Distributed under the Boost Software License, Version 1.0. 
-# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
+# Copyright 2006 Rene Rivera
+# Copyright 2003, 2004, 2005, 2006 Vladimir Prus
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
 
 set -e
 
