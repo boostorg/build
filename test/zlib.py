4fcd90347e0b54e1c66e1488280deaa643645e65
Steven Watanabe <steven@providere-consulting.com>
2013-04-06 02:11:29 +0000

    Add test with explicit name/search/include.
    [SVN r83774]

--- test/zlib.py
+++ test/zlib.py
@@ -33,6 +33,17 @@ t.run_build_system()
 t.expect_addition('bin/standalone/zlib/mock/debug/libz.so')
 t.expect_addition('bin/standalone/zlib/mock/debug/link-static/libz.a')
 
+t.rm('zlib')
+
+# Generic definitions that aren't configuration specific
+common_stuff = '''
+source_file('test.cpp', 'test.cpp')
+source_file('main.cpp', 'int main() {}')
+source_file('zlib.h.cpp', '#include <zlib.h>')
+action('-c -x c++ $main.cpp -o $main.o')
+'''
+t.write('test.cpp', 'test.cpp')
+
 # Default initialization - static library
 t.rm('bin')
 t.write("Jamroot.jam", """
@@ -41,13 +52,7 @@ using zlib ;
 exe test : test.cpp /zlib//zlib : : <link>static <link>shared ;
 """)
 
-t.write('test.cpp', 'test.cpp')
-
-MockToolset.set_expected(t, '''
-source_file('test.cpp', 'test.cpp')
-source_file('main.cpp', 'int main() {}')
-source_file('zlib.h.cpp', '#include <zlib.h>')
-action('-c -x c++ $main.cpp -o $main.o')
+MockToolset.set_expected(t, common_stuff + '''
 action('$main.o --static-lib=z -o $config.exe')
 action('-c -x c++ $zlib.h.cpp -o $zlib.h.o')
 action('-c -x c++ $test.cpp -o $test.o')
@@ -65,17 +70,47 @@ using zlib ;
 exe test : test.cpp /zlib//zlib : : <link>static <link>shared ;
 """)
 
-t.write('test.cpp', 'test.cpp')
-
-MockToolset.set_expected(t, '''
-source_file('test.cpp', 'test.cpp')
-source_file('main.cpp', 'int main() {}')
-source_file('zlib.h.cpp', '#include <zlib.h>')
-action('-c -x c++ $main.cpp -o $main.o')
+MockToolset.set_expected(t, common_stuff + '''
 action('$main.o --shared-lib=z -o $config.exe')
 action('-c -x c++ $zlib.h.cpp -o $zlib.h.o')
 action('-c -x c++ $test.cpp -o $test.o')
 action('$test.o --shared-lib=z -o $test')
+''')
+t.run_build_system()
+t.expect_addition('bin/mock/debug/test')
+t.expect_addition('bin/mock/debug/link-static/test')
+
+# Initialization in explicit location - static library
+t.rm('bin')
+t.write("Jamroot.jam", """
+path-constant here : . ;
+using zlib : : <name>myzlib <include>$(here)/zlib <search>$(here)/zlib ;
+exe test : test.cpp /zlib//zlib : : <link>static <link>shared ;
+""")
+
+t.write('zlib/zlib.h', 'zlib')
+
+MockToolset.set_expected(t, common_stuff + '''
+action('$main.o -L./zlib --static-lib=myzlib -o $config.exe')
+action('-c -x c++ $test.cpp -I./zlib -o $test.o')
+action('$test.o -L./zlib --static-lib=myzlib -o $test')
+''')
+t.run_build_system()
+t.expect_addition('bin/mock/debug/test')
+t.expect_addition('bin/mock/debug/link-static/test')
+
+# Initialization in explicit location - shared library
+t.rm('bin')
+t.write("Jamroot.jam", """
+path-constant here : . ;
+using zlib : : <name>myzlib <include>$(here)/zlib <search>$(here)/zlib ;
+exe test : test.cpp /zlib//zlib : : <link>static <link>shared ;
+""")
+
+MockToolset.set_expected(t, common_stuff + '''
+action('$main.o -L./zlib --shared-lib=myzlib -o $config.exe')
+action('-c -x c++ $test.cpp -I./zlib -o $test.o')
+action('$test.o -L./zlib --shared-lib=myzlib -o $test')
 ''')
 t.run_build_system()
 t.expect_addition('bin/mock/debug/test')
