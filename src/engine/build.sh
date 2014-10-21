4bbd414accb5aaac5d06db28441ec86a5e742592
Alain Miniussi <alain.miniussi@oca.eu>
2014-10-21 11:46:57 +0200

    Detect the intel compiler in the environement. The mentioned 'standard' path were quite old (9.0, last Intel compiler version is 15.x as on Oct 2014). I package organisation changes quite often. Keept the legacy detection though. Build in 64 bit (eems more common).

--- src/engine/build.sh
+++ src/engine/build.sh
@@ -70,8 +70,8 @@ Guess_Toolset ()
     elif test_uname IRIX64 ; then BOOST_JAM_TOOLSET=mipspro
     elif test_uname OSF1 ; then BOOST_JAM_TOOLSET=tru64cxx
     elif test_uname QNX && test_path qcc ; then BOOST_JAM_TOOLSET=qcc
-    elif test_path gcc ; then BOOST_JAM_TOOLSET=gcc
     elif test_path icc ; then BOOST_JAM_TOOLSET=intel-linux
+    elif test_path gcc ; then BOOST_JAM_TOOLSET=gcc
     elif test -r /opt/intel/cc/9.0/bin/iccvars.sh ; then
         BOOST_JAM_TOOLSET=intel-linux
         BOOST_JAM_TOOLSET_ROOT=/opt/intel/cc/9.0
@@ -142,16 +142,27 @@ case $BOOST_JAM_TOOLSET in
     ;;
 
     intel-linux)
-    if test -r /opt/intel/cc/9.0/bin/iccvars.sh ; then
-        BOOST_JAM_TOOLSET_ROOT=/opt/intel/cc/9.0/
-    elif test -r /opt/intel_cc_80/bin/iccvars.sh ; then
-        BOOST_JAM_TOOLSET_ROOT=/opt/intel_cc_80/
-    elif test -r /opt/intel/compiler70/ia32/bin/iccvars.sh ; then
-        BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler70/ia32/
-    elif test -r /opt/intel/compiler60/ia32/bin/iccvars.sh ; then
-        BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler60/ia32/
-    elif test -r /opt/intel/compiler50/ia32/bin/iccvars.sh ; then
-        BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler50/ia32/
+    which icc >/dev/null 2>&1
+    if test $? ; then
+	BOOST_JAM_CC=$(which icc)
+	echo "Found $BOOST_JAM_CC in environment"
+	BOOST_JAM_TOOLSET_ROOT=$(echo $BOOST_JAM_CC | sed -e 's/bin.*\/icc//')
+	# probably the most widespread
+	ARCH=intel64
+    else
+	echo "No intel compiler in current path"
+	echo "Look in a few old place for legacy reason"
+	if test -r /opt/intel/cc/9.0/bin/iccvars.sh ; then
+            BOOST_JAM_TOOLSET_ROOT=/opt/intel/cc/9.0/
+	elif test -r /opt/intel_cc_80/bin/iccvars.sh ; then
+            BOOST_JAM_TOOLSET_ROOT=/opt/intel_cc_80/
+	elif test -r /opt/intel/compiler70/ia32/bin/iccvars.sh ; then
+            BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler70/ia32/
+	elif test -r /opt/intel/compiler60/ia32/bin/iccvars.sh ; then
+            BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler60/ia32/
+	elif test -r /opt/intel/compiler50/ia32/bin/iccvars.sh ; then
+            BOOST_JAM_TOOLSET_ROOT=/opt/intel/compiler50/ia32/
+	fi
     fi
     if test -r ${BOOST_JAM_TOOLSET_ROOT}bin/iccvars.sh ; then
         # iccvars does not change LD_RUN_PATH. We adjust LD_RUN_PATH here in
@@ -164,9 +175,11 @@ case $BOOST_JAM_TOOLSET in
             LD_RUN_PATH="${BOOST_JAM_TOOLSET_ROOT}lib:${LD_RUN_PATH}"
         fi
         export LD_RUN_PATH
-        . ${BOOST_JAM_TOOLSET_ROOT}bin/iccvars.sh
+        . ${BOOST_JAM_TOOLSET_ROOT}bin/iccvars.sh $ARCH
     fi
-    BOOST_JAM_CC=icc
+    if test -z "$BOOST_JAM_CC" ; then
+	BOOST_JAM_CC=icc
+    fi
     ;;
 
     vacpp)
