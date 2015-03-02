d1910dad4a78b84e23b98a2f4f376cae59c9ec48
Vladimir Prus <vladimir@codesourcery.com>
2015-03-02 23:40:13 +0300

    Make websockets-to-b2 bridge handle b2 output by lines.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ var http = require('http').Server(app);
 
 var WS = require('ws').Server;
 
+var os = require('os');
+var byline = require('byline');
+
 app.get('/', function(req, res) {
     res.send('<h1>Hello, World</h1>')
 });
@@ -26,36 +29,56 @@ function sendOutput(ws, output) {
 ws.on('connection', function(ws) {
     console.log("client connected");
 
-    ws.on('message', function(xm) {
-        var m = JSON.parse(xm);
+    sendOutput(ws, "Hi there");
 
-        if (m.type === 'request') {
+    var b2 = spawn("/home/ghost/Sources/boost/tools/build/src/engine/bin.linuxx86_64/b2",
+                   ["-a", "-d0", "--python", "--server"],
+                   {
+                       cwd: "/home/ghost/Sources/boost/tools/build/example/libraries"
+                   });
 
-            if (m.request === 'build') {
+    b2.on('error', function(err) {
+        console.log(JSON.stringify(err));
+    });
 
-                var b2 = spawn("/home/ghost/Sources/boost/b2",
-                               ["-a"],
-                               {
-                                   cwd: "/home/ghost/Sources/boost/tools/build/example/hello"
-                               });
+    b2.on('exit', function(code, signal) {
+        console.log("Process exit: " + code);
+    });
 
-                b2.stdout.on('data', function(data) {
-                    if (typeof data === 'object') {
-                        data = data.toString();
-                    }
-                    console.log(JSON.stringify(data));
-                    sendOutput(ws, data);
-                });
+    b2.on('close', function(code) {
+        console.log('close: ' + code);
+    });
 
-                setTimeout(function() {
-                    sendOutput(ws, "message from server");
-                }, 1500);
-            }
-
+    byline.createStream(b2.stdout).on('data', function(line) {
+        console.log("< " + line);
+        if (typeof line === 'object') {
+            line = line.toString();
         }
 
-        console.log("got message: " + m);
+        try {
+            j = JSON.parse(line);
+            s = JSON.stringify(j);
+            ws.send(s);
+        } catch(e) {
+            console.log(e);
+        }                                
+
     });
+
+    b2.stderr.on('data', function(data) {
+        console.log("<!" + data);
+    });
+
+    //b2.stdin.write(JSON.stringify({type: 'request', request: 'build'}) + "\n");
+    
+    ws.on('message', function(data) {
+        console.log("> " + JSON.stringify(data));
+        b2.stdin.write(data + "\n");
+    });
+});
+
+ws.on('error', function(ws) {
+    console.log('socket error');
 });
 
 
