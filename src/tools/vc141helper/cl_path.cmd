00c23580afdd519fa716ee32d6dba500e6b1e991
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2017-03-19 00:59:40 +0800

    Check Visual Studio 2017+ version in script
    * Fixed `build.bat` stack overflow on some machines by splitting up to several files
    * CSharp script simplified and now will check VC++ toolset version

---
+++ src/tools/vc141helper/cl_path.cmd
@@ -0,0 +1,9 @@
+:: Copyright 2017 - Refael Ackermann
+:: Copyright 2017 - Arkady Shapkin
+:: Distributed under the Boost Software License, Version 1.0.
+:: (See accompanying file LICENSE_1_0.txt or copy at
+:: http://www.boost.org/LICENSE_1_0.txt)
+@ECHO OFF
+FOR /F "tokens=*" %%A IN ('powershell -NoProfile -ExecutionPolicy Bypass "%~dp0GetCLPath.ps1" %1 %2') DO (
+	ECHO %%A
+)
