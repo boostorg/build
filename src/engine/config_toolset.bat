99b01d131bfdebba7b7e282a325a1221109dabbc
Refael Ackermann <refack@gmail.com>
2017-06-27 11:18:54 -0400

    extra ERRORLEVEL reset

--- src/engine/config_toolset.bat
+++ src/engine/config_toolset.bat
@@ -162,6 +162,8 @@ set "_known_=1"
 :Skip_VC14
 if NOT "_%BOOST_JAM_TOOLSET%_" == "_vc141_" goto Skip_VC141
 call vswhere_usability_wrapper.cmd
+REM Reset ERRORLEVEL since from now on it's all based on ENV vars
+ver > nul 2> nul
 if "_%BOOST_JAM_TOOLSET_ROOT%_" == "__" (
     if NOT "_%VS150COMNTOOLS%_" == "__" (
         set "BOOST_JAM_TOOLSET_ROOT=%VS150COMNTOOLS%..\..\VC\"
