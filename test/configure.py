3ae87ff4966b433dc916c13bbc64b57e42d74df6
Steven Watanabe <steven@providere-consulting.com>
2017-12-19 13:30:09 -0700

    Add a new rule configure.choose that adds properties associated with the first target from a list that successfully builds.

--- test/configure.py
+++ test/configure.py
@@ -9,7 +9,7 @@
 
 import BoostBuild
 
-def test_basic():
+def test_check_target_builds():
     t = BoostBuild.Tester(use_test_config=0)
     t.write("Jamroot", """
 import configure ;
@@ -67,4 +67,55 @@ obj bar : foo.cpp :
 
     t.cleanup()
 
-test_basic()
+def test_choose():
+    t = BoostBuild.Tester(use_test_config=0)
+    t.write("Jamroot", """
+import configure ;
+obj pass : pass.cpp ;
+obj fail : fail.cpp ;
+explicit pass fail ;
+obj foo : foo.cpp :
+  [ configure.choose "which one?" : fail <define>FAIL : pass <define>PASS ] ;
+""")
+    t.write("pass.cpp", "void f() {}\n")
+    t.write("fail.cpp", "#error fail.cpp\n")
+    t.write("foo.cpp", """
+#ifndef PASS
+#error PASS not defined
+#endif
+#ifdef FAIL
+#error FAIL is defined
+#endif
+""")
+    t.run_build_system()
+    t.expect_output_lines([
+        "    - which one?               : pass"])
+    t.expect_addition("bin/$toolset/debug*/pass.obj")
+    t.expect_addition("bin/$toolset/debug*/foo.obj")
+    t.expect_nothing_more()
+
+    # An up-to-date build should use the cache
+    t.run_build_system()
+    t.expect_output_lines([
+        "    - which one?               : pass (cached)"])
+    t.expect_nothing_more()
+
+    # -a should re-run everything, including configuration checks
+    t.run_build_system(["-a"])
+    t.expect_output_lines([
+        "    - which one?               : pass"])
+    t.expect_touch("bin/$toolset/debug*/pass.obj")
+    t.expect_touch("bin/$toolset/debug*/foo.obj")
+    t.expect_nothing_more()
+    
+    # --reconfigure should re-run configuration checks only
+    t.run_build_system(["--reconfigure"])
+    t.expect_output_lines([
+        "    - which one?               : pass"])
+    t.expect_touch("bin/$toolset/debug*/pass.obj")
+    t.expect_nothing_more()
+
+    t.cleanup()
+
+test_check_target_builds()
+test_choose()
