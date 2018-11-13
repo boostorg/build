6f235b53bd7ff64563c8201603c2abf1025b96d7
Rene Rivera <grafikrobot@gmail.com>
2018-11-12 18:48:19 -0600

    Account for target-os in build path.

--- test/remove_requirement.py
+++ test/remove_requirement.py
@@ -57,10 +57,10 @@ exe hello : hello.cpp ;
 
 t.run_build_system()
 
-t.expect_addition("sub/bin/$toolset/debug/link-static*/hello.exe")
-t.expect_addition("sub2/bin/$toolset/debug/link-static*/hello.exe")
-t.expect_addition("sub3/bin/$toolset/debug/threading-multi*/hello.exe")
-t.expect_addition("sub4/bin/$toolset/debug/threading-multi*/hello.exe")
+t.expect_addition("sub/bin/$toolset/debug*/link-static*/hello.exe")
+t.expect_addition("sub2/bin/$toolset/debug*/link-static*/hello.exe")
+t.expect_addition("sub3/bin/$toolset/debug*/threading-multi*/hello.exe")
+t.expect_addition("sub4/bin/$toolset/debug*/threading-multi*/hello.exe")
 
 t.rm(".")
 
