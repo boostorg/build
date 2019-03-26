c7c1d19e6148b703c03e89db411a09ccdb487e7a
Rene Rivera <grafikrobot@gmail.com>
2019-03-25 23:18:13 -0500

    Fix popen redef error and remove old mingw as it doesn't have popen.

--- src/engine/builtins.c
+++ src/engine/builtins.c
@@ -2410,7 +2410,9 @@ PyObject * bjam_caller( PyObject * self, PyObject * args )
 #ifdef HAVE_POPEN
 
 #if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__MINGW64__) || defined(__MINGW32__)
+    #undef popen
     #define popen windows_popen_wrapper
+    #undef pclose
     #define pclose _pclose
 
     /*
