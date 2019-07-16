8b662f887e70d91f2f538c2864a5052300f57935
Rene Rivera <grafikrobot@gmail.com>
2019-07-16 16:45:34 -0600

    Add documentation comments for binder API.

--- src/engine/bind.h
+++ src/engine/bind.h
@@ -9,40 +9,115 @@
 #include "config.h"
 #include <functional>
 
+/** // tag::binder[]
+
+= Binder
+
+The B2 C++ native engine and system is reflected to various other languages
+through bindings of classes and functions. This is accomplished through custom
+C++ 11 reflection and non-intrusive declaration of a binding API.
+
+ */ // end::binder[]
+
 namespace b2
 {
 namespace bind
 {
 
+/** // tag::binder_type[]
+
+== `b2::bind::type_`
+
+Tag wrapper definition to specify a type to one of the binder specifications.
+For example:
+
+```
+binder.def_class("system_info", type_<b2::system_info>());
+```
+
+ */ // end::binder_type[]
 template <typename T>
 struct type_
 {
     typedef T type;
 };
 
+/** // tag::binder_init[]
+
+== `b2::bind::init_`
+
+Tag wrapper definition to specify the types of arguments for init methods,
+i.e. constructors, to a binder definition.
+For example:
+
+```
+binder.def_class("system_info", type_<b2::system_info>())
+    .def(init_<>());
+```
+
+ */ // end::binder_init[]
 template <typename... Args>
 struct init_
 {
 };
 
+/** // tag::binder_module[]
+
+== `b2::bind::module_`
+
+The base type for a module binding. Modules are a collection of class and other
+binding declarations. A module is specified as a concrete class with at minimum
+a `module_name` and a definition method
+(`template <class Binder> void def(Binder & binder)`).
+For example:
+
+```
+struct sysinfo_module
+    : b2::bind::module_<sysinfo_module>
+{
+    const char *module_name = "sysinfo";
+
+    template <class Binder>
+    void def(Binder & binder)
+    {
+        // ...
+    }
+};
+```
+
+ */ // end::binder_module[]
 template <class Module>
 struct module_
 {
+    // Alias shorthand for `b2::bind::type_` to avoid namespace qualification.
     template <class T>
     using type_ = ::b2::bind::type_<T>;
 
+    // Alias shorthand for `b2::bind::init_` to avoid namespace qualification.
     template <class... A>
     using init_ = ::b2::bind::init_<A...>;
 
-    module_()
-    {
-    }
-
+    // Get the sub-typed, from CRTP, `*this` reference.
     Module &self() { return *static_cast<Module *>(this); }
 };
 
-namespace detail
-{
+/** // tag::binder_class[]
+
+== `b2::bind::class_`
+
+The `class_` type provides for defining class level members, i.e. methods. The
+`class_` type is returned by the `binder_::def_class(..)` to give a quick, and
+scoped, manner to define complete class bindings.
+For example:
+
+```
+binder.def_class("system_info", type_<b2::system_info>())
+    .def(init_<>())
+    .def("cpu_core_count", &b2::system_info::cpu_core_count)
+    .def("cpu_thread_count", &b2::system_info::cpu_thread_count);
+```
+
+ */ // end::binder_class[]
 template <class Class, class Binder>
 struct class_
 {
@@ -54,25 +129,71 @@ struct class_
     {
     }
 
+    /** // tag::binder_class[]
+
+    === `b2::bind::class_::def(init_<Args...> init_args) -> class_ &`
+
+    Defines an init method, i.e. constructor, with the given `init_args` types.
+    The given `init_args` will match an appropriate constructor. And the
+    constructor will be called from the bound language as appropriate for the
+    language. For example for `jam` it will call the `__init__` rule.
+
+     */ // end::binder_class[]
+    template <class... Args>
+    class_ &def(init_<Args...> init_args)
+    {
+        // Forward to the language specific binder.
+        binder.def_init(this->name, (Class *)nullptr, init_args);
+        return *this;
+    }
+
+    /** // tag::binder_class[]
+
+    === `b2::bind::class_::def(const char *name, F function) -> class_ &`
+
+    Defines a method which it bound with the given `name` which calls the given
+    `function`.
+
+     */ // end::binder_class[]
     template <class F>
     class_ &def(const char *name, F function)
     {
+        // Forward to the language specific binder.
         binder.def_method(this->name, name, function);
         return *this;
     }
-
-    template <class... Args>
-    class_ &def(init_<Args...> i)
-    {
-        binder.def_init(this->name, (Class*) nullptr, i);
-        return *this;
-    }
 };
-} // namespace detail
 
+/** // tag::binder_binder[]
+
+== `b2::bind::binder_`
+
+Interface for a language agnostic binder. This is passed to the module
+definition function (`template <class B> def(B & binder)`). What is passed
+is a language specific subclass that will generate the needed bindings for
+that language.
+
+ */ // end::binder_binder[]
 template <class Binder>
 struct binder_
 {
+    /** // tag::binder_binder[]
+
+    === `b2::bind::binder_::def_class`
+
+    Declares the definition of a class, given in the `type_` wrapper, in the
+    module.
+
+     */ // end::binder_binder[]
+    template <class Class>
+    class_<Class, Binder> def_class(const char *name, type_<Class> class_type)
+    {
+        self().bind_class(current_module_name, name, class_type);
+        return class_<Class, Binder>(name, self());
+    }
+
+    // Internal..
+
     Binder &self() { return *static_cast<Binder *>(this); }
 
     template <class Module>
@@ -84,30 +205,24 @@ struct binder_
         return self();
     }
 
-    template <class Class>
-    detail::class_<Class, Binder> def_class(const char *name, bind::type_<Class> class_type)
-    {
-        self().bind_class(current_module_name, name, class_type);
-        return detail::class_<Class, Binder>(name, self());
-    }
-
+    // Respond to a method definition of a class. This calls the subclass
+    // method `bind_method(module_name, class_name, method_name, function)`.
     template <class Function>
     void def_method(const char *class_name, const char *method_name, Function f)
     {
         self().bind_method(current_module_name, class_name, method_name, f);
     }
 
+    // Respond to a constructor definition of a class. This calls the subclass
+    // method `bind_init(module_name, class_name, class_nullptr, init)`.
     template <class Class, class Init>
-    void def_init(const char *class_name, Class * c, Init i)
+    void def_init(const char *class_name, Class *c, Init i)
     {
         self().bind_init(current_module_name, class_name, c, i);
     }
 
 protected:
     const char *current_module_name = nullptr;
-
-    // template <class Function>
-    // void bind_method(const char *module_name, const char *class_name, const char *method_name, Function f);
 };
 
 }; // namespace bind
