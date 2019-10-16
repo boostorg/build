1a29945805c6c6888a28822354d14bd6c611bb7f
Rene Rivera <grafikrobot@gmail.com>
2019-10-15 21:08:23 -0500

    Use b2 regex engine as it's portable and faster than std.

--- src/engine/modules/mod_regex.cpp
+++ src/engine/modules/mod_regex.cpp
@@ -4,16 +4,23 @@
  */
 
 #include "mod_regex.h"
-#include <regex>
+#include "../mem.h"
+#include "../regexp.h"
+#include <numeric>
+#include <memory>
 
 using namespace b2;
 
 namespace
 {
+using regex_ptr = std::unique_ptr<regexp, std::function<void(regexp *)>>;
+
 template <typename Pattern>
-std::regex make_regex(Pattern &&pattern)
+regex_ptr make_regex(Pattern &&pattern)
 {
-    return std::regex{std::forward<Pattern>(pattern), std::regex::extended};
+    return regex_ptr(
+        regcomp(pattern.c_str()),
+        [](regexp *r) { BJAM_FREE(r); });
 }
 } // namespace
 
@@ -22,21 +29,22 @@ std::vector<string_t> b2::regex_split(
 {
     std::vector<string_t> result;
     string_t string{std::get<0>(string_separator)};
-    std::regex separator = make_regex(std::get<1>(string_separator));
-    std::sregex_token_iterator tokens_i{string.cbegin(), string.cend(), separator, {-1, 0}};
-    std::sregex_token_iterator tokens_e{};
-    std::sregex_token_iterator tokens_z{};
-    while (tokens_i != tokens_e)
+    auto re = make_regex(std::get<1>(string_separator));
+    auto pos = string.c_str();
+    auto prev = pos;
+    while (regexec(re.get(), pos))
     {
-        result.push_back(*tokens_i++);
-        tokens_z = tokens_i;
-        if (tokens_i != tokens_e)
-            tokens_i++;
+        result.emplace_back(prev, re->startp[0]);
+        prev = re->endp[0];
+        /* Handle empty matches */
+        if (*pos == '\0')
+            break;
+        else if (pos == re->endp[0])
+            pos++;
+        else
+            pos = re->endp[0];
     }
-    if (tokens_z != tokens_e)
-    {
-        result.emplace_back("");
-    }
+    result.push_back(pos);
     return result;
 }
 
@@ -44,24 +52,24 @@ std::vector<string_t> b2::regex_split_each(
     const std::vector<string_t> &to_split, const string_t &separator)
 {
     std::vector<string_t> result;
-    std::regex separator_re = make_regex(separator);
-    std::sregex_token_iterator tokens_e{};
+    auto re = make_regex(separator);
     for (const string_t &string : to_split)
     {
-        std::sregex_token_iterator tokens_i{
-            string.cbegin(), string.cend(), separator_re, {-1, 0}};
-        std::sregex_token_iterator tokens_z{};
-        while (tokens_i != tokens_e)
+        auto pos = string.c_str();
+        auto prev = pos;
+        while (regexec(re.get(), pos))
         {
-            result.push_back(*tokens_i++);
-            tokens_z = tokens_i;
-            if (tokens_i != tokens_e)
-                tokens_i++;
+            result.emplace_back(prev, re->startp[0]);
+            prev = re->endp[0];
+            /* Handle empty matches */
+            if (*pos == '\0')
+                break;
+            else if (pos == re->endp[0])
+                pos++;
+            else
+                pos = re->endp[0];
         }
-        if (tokens_z != tokens_e)
-        {
-            result.emplace_back("");
-        }
+        result.push_back(pos);
     }
     return result;
 }
@@ -70,19 +78,25 @@ std::vector<string_t> b2::regex_match(
     const string_t &pattern, const string_t &string,
     const std::vector<int_t> &indices)
 {
-    std::regex pattern_re = make_regex(pattern);
-    if (indices.empty())
+    std::vector<string_t> result;
+    auto re = make_regex(pattern);
+    if (regexec(re.get(), string.c_str()))
     {
-        std::smatch match;
-        if (std::regex_search(string, match, pattern_re))
-            return {++match.cbegin(), match.cend()};
-        return {};
+        if (!indices.empty())
+        {
+            for (int_t index : indices)
+            {
+                if (index < NSUBEXP)
+                    result.emplace_back(re->startp[index], re->endp[index]);
+            }
+        }
+        else
+        {
+            for (int_t index = 1; index < NSUBEXP && re->startp[index] != nullptr; ++index)
+                result.emplace_back(re->startp[index], re->endp[index]);
+        }
     }
-    else
-        return {
-            std::sregex_token_iterator{
-                string.cbegin(), string.cend(), pattern_re, indices},
-            std::sregex_token_iterator{}};
+    return result;
 }
 
 std::vector<string_t> b2::regex_transform(
@@ -91,29 +105,26 @@ std::vector<string_t> b2::regex_transform(
     const std::vector<int_t> &indices)
 {
     std::vector<string_t> result;
-    std::regex pattern_re = make_regex(pattern);
-    std::sregex_token_iterator tokens_e{};
-    if (indices.empty())
+    auto re = make_regex(pattern);
+    if (!indices.empty())
     {
         for (const string_t &string : list)
         {
-            std::sregex_token_iterator tokens_i{
-                string.cbegin(), string.cend(), pattern_re, 1};
-            if (tokens_i != tokens_e && tokens_i->length() > 0)
-                result.push_back(*tokens_i);
+            if (regexec(re.get(), string.c_str()))
+                for (int_t index : indices)
+                {
+                    if (index < NSUBEXP && re->startp[index] < re->endp[index])
+                        result.emplace_back(re->startp[index], re->endp[index]);
+                }
         }
     }
     else
     {
         for (const string_t &string : list)
         {
-            std::sregex_token_iterator tokens_i{
-                string.cbegin(), string.cend(), pattern_re, indices};
-            for (; tokens_i != tokens_e; ++tokens_i)
-            {
-                if (tokens_i->length() > 0)
-                    result.push_back(*tokens_i);
-            }
+            if (regexec(re.get(), string.c_str()))
+                if (re->startp[1] != nullptr && re->startp[1] < re->endp[1])
+                    result.emplace_back(re->startp[1], re->endp[1]);
         }
     }
     return result;
