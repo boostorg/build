02cf118d80dfd748b4ba132e06a8e18426472fff
Rene Rivera <grafikrobot@gmail.com>
2021-02-20 23:10:30 -0600

    Version 4.4.2 as first for BFG.

--- src/engine/patchlevel.h
+++ src/engine/patchlevel.h
@@ -5,7 +5,7 @@
  */
 /*
 This file is ALSO:
-Copyright 2018-2019 Rene Rivera
+Copyright 2018-2021 Rene Rivera
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.txt or copy at https://www.bfgroup.xyz/b2/LICENSE.txt)
 */
@@ -13,4 +13,4 @@ Distributed under the Boost Software License, Version 1.0.
 
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 4
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
