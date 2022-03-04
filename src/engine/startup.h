d66a79b6982fe654b89e5b94c503dfb63acb918b
Rene Rivera <grafikrobot@gmail.com>
2022-03-03 20:51:43 -0600

    Add b2::clean_exit to replace plain exit/abort.

--- src/engine/startup.h
+++ src/engine/startup.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 René Ferdinand Rivera Morell
+Copyright 2020-2022 René Ferdinand Rivera Morell
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.txt or https://www.bfgroup.xyz/b2/LICENSE.txt)
 */
@@ -10,14 +10,29 @@ Distributed under the Boost Software License, Version 1.0.
 #include "config.h"
 #include "frames.h"
 
-namespace b2
+namespace b2 {
+namespace startup {
+    void load_builtins();
+    LIST* builtin_boost_build(FRAME* frame, int flags);
+    bool bootstrap(FRAME* frame);
+}
+
+enum class exit_result : int
 {
-    namespace startup
-    {
-        void load_builtins();
-        LIST *builtin_boost_build(FRAME *frame, int flags);
-        bool bootstrap(FRAME *frame);
-    }
+    success = EXIT_SUCCESS,
+    failure = EXIT_FAILURE
+};
+
+inline void clean_exit(int exit_code)
+{
+    throw (exit_result)exit_code;
+}
+
+inline void clean_exit(exit_result exit_code)
+{
+    throw exit_code;
+}
+
 } // namespace b2
 
 #endif
