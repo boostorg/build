5a7c84268541a964e3c1f86e131fe09d222cca58
Rene Rivera <grafikrobot@gmail.com>
2022-04-02 12:07:16 -0500

    Fix use-after-free error for copied FRAME objects.

--- src/engine/frames.h
+++ src/engine/frames.h
@@ -35,6 +35,23 @@ struct frame
 
     inline frame() { frame_init( this ); }
     inline ~frame() { frame_free( this ); }
+    inline frame(const frame & other)
+        : prev(other.prev)
+        , prev_user(other.prev_user)
+        , module(other.module)
+        , file(other.file)
+        , line(other.line)
+        , rulename(other.rulename)
+        #ifdef JAM_DEBUGGER
+        , function(other.function)
+        #endif
+    {
+        lol_init(args);
+        for (int32_t a = 0; a < other.args->count; ++a)
+        {
+            lol_add(args, list_copy(other.args->list[a]));
+        }
+    }
 };
 
 
