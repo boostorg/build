adf175f6376adba9ae428bc2ad49655fcb59eba9
Rene Rivera <grafikrobot@gmail.com>
2022-04-27 18:03:39 -0500

    Instrospect examples build products.

---
+++ .ci/b2_example.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+pushd $1
+$HOME/temp/.b2/bin/b2 -d+2 ${B2_ARGS}
+find . -executable -type f -o -name "*.o" -o -name "*.a" -o -name "*.so" -o -name "*.dylib" -exec file '{}' '+'
+popd
+
+# Use, modification, and distribution are
+# subject to the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE.txt)
+#
+# Copyright Rene Rivera 2022.
