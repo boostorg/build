d263ba035de95cca45033fd2668bbe117f3d4370
Rene Rivera <grafikrobot@gmail.com>
2022-04-28 08:35:46 -0500

    Avoid using bash pushd.

--- .ci/b2_example.sh
+++ .ci/b2_example.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-pushd $1
+cdir=${PWD}
+cd $1
 $HOME/temp/.b2/bin/b2 -d+2 ${B2_ARGS}
 echo "-----"
 find . -perm -111 -type f -exec file "{}" "+"
@@ -15,7 +16,7 @@ find . -name "*.so" -exec file "{}" "+"
 echo "-----"
 find . -name "*.dylib" -exec file "{}" "+"
 echo "-----"
-popd
+cd ${cdir}
 
 # Use, modification, and distribution are
 # subject to the Boost Software License, Version 1.0. (See accompanying
