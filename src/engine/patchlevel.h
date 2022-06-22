536cdbc4cbbec460c3f2bb7474f1225fe9ab9e4e
Rene Rivera <grafikrobot@gmail.com>
2022-06-21 23:39:56 -0500

    Bump to version 5.0.

--- src/engine/patchlevel.h
+++ src/engine/patchlevel.h
@@ -3,14 +3,15 @@
  *
  * This file is part of Jam - see jam.c for Copyright information.
  */
+
 /*
 This file is ALSO:
 Copyright 2018-2022 Rene Rivera
 Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE.txt or copy at https://www.bfgroup.xyz/b2/LICENSE.txt)
+(See accompanying file LICENSE.txt or copy at
+https://www.bfgroup.xyz/b2/LICENSE.txt)
 */
 
-
-#define VERSION_MAJOR 4
-#define VERSION_MINOR 9
-#define VERSION_PATCH 1
+#define VERSION_MAJOR 5
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
