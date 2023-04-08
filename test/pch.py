7cc58a89ec5868320d50be4ad1b974de4a79260d
Nikita Kniazev <nok.raven@gmail.com>
2023-04-08 22:00:47 +0300

    msvc: fix same directory pch header (#231)
    Pass to `-Yc` path as was typed by user (not exactly so, B2 automatically turns / to \ on Windows, so force the / in variable expansion).

--- test/pch.py
+++ test/pch.py
@@ -15,12 +15,15 @@ t = BoostBuild.Tester()
 t.write("jamroot.jam", """
 import pch ;
 project : requirements <warnings-as-errors>on ;
-cpp-pch pch : a/pch.hpp ;
-cpp-pch pch-afx : a/pch.hpp : <define>HELLO ;
-cpp-pch pch-msvc-source : a/pch.hpp : <toolset>msvc:<source>a/pch.cpp ;
+cpp-pch pch : pch.hpp ;
+cpp-pch pch-afx : pch.hpp : <define>HELLO ;
+cpp-pch pch-msvc-source : pch.hpp : <toolset>msvc:<source>pch.cpp ;
 exe hello : hello.cpp pch ;
 exe hello-afx : hello-afx.cpp pch-afx : <define>HELLO ;
 exe hello-msvc-source : hello-msvc-source.cpp pch-msvc-source ;
+
+cpp-pch subdir-pch : a/pch_different.hpp ;
+exe hello-subdir : a/hello.cpp subdir-pch ;
 """)
 
 pch_content = """\
@@ -31,13 +34,13 @@ public:
     TestClass( int, int ) {}
 };
 """
-t.write("a/pch.hpp", pch_content)
+t.write("pch.hpp", pch_content)
+t.copy("pch.hpp", "a/pch_different.hpp")
 
-t.write("a/pch.cpp", """#include <pch.hpp>
+t.write("pch.cpp", """#include <pch.hpp>
 """)
 
-toolset = BoostBuild.get_toolset()
-for name in ("hello.cpp", "hello-afx.cpp", "hello-msvc-source.cpp"):
+for name in ("hello.cpp", "hello-afx.cpp", "hello-msvc-source.cpp", "a/hello.cpp", "a/hello-rel.cpp"):
     t.write(name, """int main() { TestClass c(1, 2); }
 """)
 
@@ -45,6 +48,7 @@ t.run_build_system()
 t.expect_addition("bin/$toolset/debug*/hello.exe")
 t.expect_addition("bin/$toolset/debug*/hello-afx.exe")
 t.expect_addition("bin/$toolset/debug*/hello-msvc-source.exe")
+t.expect_addition("bin/$toolset/debug*/hello-subdir.exe")
 
 
 # Now make the header unusable, replace its content with some garbage, but
@@ -52,10 +56,12 @@ t.expect_addition("bin/$toolset/debug*/hello-msvc-source.exe")
 # B2 will not recreate PCH, and compiler will happily use pre-compiled
 # header, not noticing that the real header is bad.
 
-t.rename("a/pch.hpp", "a/pch.hpp.orig")
-s = "THIS WILL NOT COMPILE. "
-t.write("a/pch.hpp", s + (len(pch_content) - len(s)) * 'x')
-t.copy_timestamp("a/pch.hpp.orig", "a/pch.hpp")
+t.rename("pch.hpp", "pch.hpp.orig")
+s = """#error PCH REBUILD HAPPEND
+THIS WILL NOT COMPILE
+"""
+t.write("pch.hpp", s + (len(pch_content) - len(s)) * 'x')
+t.copy_timestamp("pch.hpp.orig", "pch.hpp")
 
 t.rm("bin/$toolset/debug*/hello.obj")
 t.rm("bin/$toolset/debug*/hello-afx.obj")
@@ -65,5 +71,24 @@ t.run_build_system()
 t.expect_addition("bin/$toolset/debug*/hello.obj")
 t.expect_addition("bin/$toolset/debug*/hello-afx.obj")
 t.expect_addition("bin/$toolset/debug*/hello-msvc-source.obj")
+
+t.rm("bin")
+t.copy("a/pch_different.hpp", "pch.hpp")
+t.rename("a", "b")
+t.write("b/jamfile.jam", """\
+import pch ;
+project : requirements <warnings-as-errors>on ;
+cpp-pch pch : pch_different.hpp ;
+exe hello : hello.cpp pch ;
+cpp-pch pch-rel : ../pch.hpp ;
+exe hello-rel : hello-rel.cpp pch-rel ;
+""")
+t.run_build_system(["-d+2", "b"])
+t.expect_addition("b/bin/$toolset/debug*/hello.exe")
+t.expect_addition("b/bin/$toolset/debug*/hello-rel.exe")
+t.rm("b/bin")
+t.run_build_system(["-d+2"], subdir="b")
+t.expect_addition("b/bin/$toolset/debug*/hello.exe")
+t.expect_addition("b/bin/$toolset/debug*/hello-rel.exe")
 
 t.cleanup()
