31ff337943121512ade09c01bde824716a6380b0
Rene Rivera <grafikrobot@gmail.com>
2024-01-13 09:37:37 -0600

    Bump version for next release and add docs.

--- src/engine/patchlevel.h
+++ src/engine/patchlevel.h
@@ -6,12 +6,12 @@
 
 /*
 This file is ALSO:
-Copyright 2018-2022 Rene Rivera
+Copyright 2018-2024 Rene Rivera
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.txt or copy at
 https://www.bfgroup.xyz/b2/LICENSE.txt)
 */
 
 #define VERSION_MAJOR 5
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_PATCH 0
