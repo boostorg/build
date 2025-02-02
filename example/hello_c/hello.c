bf453ee2b27c718756daa34125280f68dd091670
Rene Rivera <grafikrobot@gmail.com>
2025-02-02 16:57:22 -0600

    Add generic cc toolset.

---
+++ example/hello_c/hello.c
@@ -0,0 +1,15 @@
+/*
+Copyright 2025 René Ferdinand Rivera Morell
+Distributed under the Boost Software License, Version 1.0.
+See https://www.bfgroup.xyz/b2/LICENSE.txt
+*/
+
+// tag::source[]
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello!\n");
+	return 0;
+}
+// end::source[]
